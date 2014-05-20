24f3b8edb0577a952598b0abffe04a3ba3756c9d
Jake Gordon <jacob.gordon@Emerson.com>
2014-05-19 23:35:47 -0400

    I think i'm done testing BusinessLogic.Logic.CompletedGame. Still needs refactoring though.

--- Source/BusinessLogic/Logic/CompletedGame.cs
+++ Source/BusinessLogic/Logic/CompletedGame.cs
@@ -10,7 +10,12 @@ namespace BusinessLogic.Logic
 {
     public class CompletedGame
     {
-        NerdScorekeeperDbContext dbContext = null;
+        internal const string EXCEPTION_MESSAGE_MUST_PASS_VALID_GAME_DEFINITION_ID = "Must pass a valid GameDefinitionId.";
+        internal const string EXCEPTION_MESSAGE_MUST_PASS_AT_LEAST_ONE_PLAYER = "Must pass in at least one player";
+        internal const string EXCEPTION_MESSAGE_GAME_MUST_HAVE_A_WINNER = "The game must have at least one winner (GameRank = 1)";
+        internal const string EXCEPTION_MESSAGE_GAME_MUST_NOT_HAVE_A_GAP_IN_RANKS = "The game must not have gaps in the ranks. E.g. 1,1,2,3 is valid but 1,1,3,4 is not.";
+
+        private NerdScorekeeperDbContext dbContext = null;
 
         public CompletedGame(NerdScorekeeperDbContext context)
         {
@@ -21,12 +26,34 @@ namespace BusinessLogic.Logic
         {
             if(newlyCompletedGame.GameDefinitionId == 0)
             {
-                throw new ArgumentException("Must pass a valid GameDefinitionId.");
+                throw new ArgumentException(EXCEPTION_MESSAGE_MUST_PASS_VALID_GAME_DEFINITION_ID);
             }
             
             if(newlyCompletedGame.PlayerRanks.Count < 1)
             {
-                throw new ArgumentException("Must pass in at least one player");
+                throw new ArgumentException(EXCEPTION_MESSAGE_MUST_PASS_AT_LEAST_ONE_PLAYER);
+            }
+
+            if(newlyCompletedGame.PlayerRanks.FirstOrDefault(x => x.GameRank == 1) == null)
+            {
+                throw new ArgumentException(EXCEPTION_MESSAGE_GAME_MUST_HAVE_A_WINNER);
+            }
+
+            int numberOfPlayers = newlyCompletedGame.PlayerRanks.Count();
+
+            int numberOfPlayersCoveredSoFar = 0;
+
+            //TODO review with Clean Code book club
+            for (int i = 1; numberOfPlayersCoveredSoFar < numberOfPlayers; i++)
+            {
+                int numberOfPlayersWithThisRank = newlyCompletedGame.PlayerRanks.Count(x => x.GameRank == i);
+
+                if (numberOfPlayersWithThisRank == 0)
+                {
+                    throw new ArgumentException(EXCEPTION_MESSAGE_GAME_MUST_NOT_HAVE_A_GAP_IN_RANKS);
+                }
+
+                numberOfPlayersCoveredSoFar += numberOfPlayersWithThisRank;
             }
 
             var playerList = newlyCompletedGame.PlayerRanks.Select(x =>  new Player() { Id = x.PlayerId} ).ToList();
@@ -34,7 +61,7 @@ namespace BusinessLogic.Logic
             PlayedGame playedGame = new PlayedGame()
             {
                 GameDefinitionId = newlyCompletedGame.GameDefinitionId,
-                NumberOfPlayers = newlyCompletedGame.PlayerRanks.Count(),
+                NumberOfPlayers = numberOfPlayers,
                 Players = playerList
             };
 
--- Tests/BusinessLogic.Tests/UnitTests/Logic/CompletedGameTests.cs
+++ Tests/BusinessLogic.Tests/UnitTests/Logic/CompletedGameTests.cs
@@ -25,7 +25,7 @@ namespace BusinessLogic.Tests.UnitTests.Logic
         }
 
         [Test]
-        [ExpectedException(typeof(ArgumentException))]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = CompletedGame.EXCEPTION_MESSAGE_MUST_PASS_AT_LEAST_ONE_PLAYER)]
         public void ItRequiresMoreThanOnePlayer()
         {
             NewlyCompletedGame newlyCompletedGameThatHasNoPlayers = new NewlyCompletedGame();
@@ -36,12 +36,53 @@ namespace BusinessLogic.Tests.UnitTests.Logic
         }
 
         [Test]
-        [ExpectedException(typeof(ArgumentException))]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = CompletedGame.EXCEPTION_MESSAGE_MUST_PASS_VALID_GAME_DEFINITION_ID)]
         public void ItRequiresAGame()
         {
             NewlyCompletedGame newlyCompletedGameThatHasNoGameDefinitionId = new NewlyCompletedGame();
             newlyCompletedGameThatHasNoGameDefinitionId.PlayerRanks = new List<PlayerRank>() { new PlayerRank() { PlayerId = 2, GameRank = 1 } };
+
             playedGameLogic.CreatePlayedGame(newlyCompletedGameThatHasNoGameDefinitionId);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = CompletedGame.EXCEPTION_MESSAGE_GAME_MUST_HAVE_A_WINNER)]
+        public void ItRequiresAWinner()
+        {
+            NewlyCompletedGame newlyCompletedGameWithoutAWinner = new NewlyCompletedGame() { GameDefinitionId = 1 };
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            playerRanks.Add(new PlayerRank() { PlayerId = 1, GameRank = 2 });
+            playerRanks.Add(new PlayerRank() { PlayerId = 1, GameRank = 3 });
+            newlyCompletedGameWithoutAWinner.PlayerRanks = playerRanks;
+
+            playedGameLogic.CreatePlayedGame(newlyCompletedGameWithoutAWinner);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = CompletedGame.EXCEPTION_MESSAGE_GAME_MUST_NOT_HAVE_A_GAP_IN_RANKS)]
+        public void ItThrowsArgumentExceptionIfMissingARankInbetweenTwoOtherRanks()
+        {
+            NewlyCompletedGame newlyCompletedGameWithUncontiguousGameRanks = new NewlyCompletedGame() { GameDefinitionId = 1 };
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            playerRanks.Add(new PlayerRank() { PlayerId = 1, GameRank = 1 });
+            playerRanks.Add(new PlayerRank() { PlayerId = 2, GameRank = 3 });
+            newlyCompletedGameWithUncontiguousGameRanks.PlayerRanks = playerRanks;
+
+            playedGameLogic.CreatePlayedGame(newlyCompletedGameWithUncontiguousGameRanks);
+        }
+
+        [Test]
+        public void ItAcceptsAGameWithRanksOneTwoAndThreeRanks()
+        {
+            NewlyCompletedGame newlyCompletedGameWithUncontiguousGameRanks = new NewlyCompletedGame() { GameDefinitionId = 1 };
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            playerRanks.Add(new PlayerRank() { PlayerId = 1, GameRank = 1 });
+            playerRanks.Add(new PlayerRank() { PlayerId = 2, GameRank = 1 });
+            playerRanks.Add(new PlayerRank() { PlayerId = 3, GameRank = 2 });
+            playerRanks.Add(new PlayerRank() { PlayerId = 4, GameRank = 3 });
+            newlyCompletedGameWithUncontiguousGameRanks.PlayerRanks = playerRanks;
+
+            playedGameLogic.CreatePlayedGame(newlyCompletedGameWithUncontiguousGameRanks);
         }
     }
 }
