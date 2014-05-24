288a7121e477d1753c410b434153ea2db16152c2
Jake Gordon <jacob.gordon@Emerson.com>
2014-05-24 09:31:17 -0400

    started to build a new PlayerLogic class for getting detailed player info. Also stubbed out a snippet xml for setting up new integration test classes.

---
+++ Source/BusinessLogic/Logic/PlayerLogic.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogic.DataAccess;
+using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Logic
+{
+    public class PlayerLogic
+    {
+        private NerdScorekeeperDbContext dbContext = null;
+
+        public PlayerLogic(NerdScorekeeperDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public Player GetPlayerDetails(int playerID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/BusinessLogic.Tests/IntegrationTests/Logic/PlayerLogicIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLogic.DataAccess;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests.IntegrationTests.Logic
+{
+    [TestFixture]
+    public class PlayerLogicIntegrationTests
+    {
+        private NerdScorekeeperDbContext dbContext;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            dbContext = new NerdScorekeeperDbContext();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+
+    }
+}
