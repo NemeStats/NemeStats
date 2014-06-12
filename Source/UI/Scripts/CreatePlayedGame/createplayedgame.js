9f7049e2ca524243db6e9ff3314e6972455f73cd
Jake Gordon <jacob.gordon@Emerson.com>
2014-06-12 07:16:51 -0400

    switched over to Toshos version of createplayedgame.js

--- Source/UI/Scripts/CreatePlayedGame/createplayedgame.js
+++ Source/UI/Scripts/CreatePlayedGame/createplayedgame.js
@@ -1,44 +1,101 @@
-﻿var gbl_playerRank = 1;
-var gbl_playerIndex = 0;
+﻿    //Usings
+    window.Views = window.Views || {};
+    window.Views.PlayedGame = Views.PlayedGame || {};
 
-$(document).ready(function () {
-    $("#rankedPlayers").sortable({ stop: onReorder });
-    $("#Players").change(addPlayer);
-});
+    //Initialization
+    window.Views.PlayedGame.CreatePlayedGame = function () {
+        this._playerRank = 1;
+        this._playerIndex = 0;
+        this.$rankedPlayers = null;
+        this.$rankedPlayersListItem = null;
+        this.$playerId = null;
+        this.$players = null;
+        this.$playerFormData = null;
+    };
 
-function onReorder()
-{
-    $("#rankedPlayers li").each(function (index, value) {
-        var listItem = $(value);
-        var playerId = listItem.attr("data-playerId");
-        var rank = index + 1;
-        $("#" + playerId).val(rank);
+    //Implementation
+    window.Views.PlayedGame.CreatePlayedGame.prototype = {
 
-        var playerName = listItem.attr("data-playerName");
-        listItem.html(generatePlayerRankListItemString(playerName, rank));
+        //Method definitions
+        init: function () {
+            //Fields
+            var parent = this;
+            this.$rankedPlayers = $("#rankedPlayers");
+            this.$players = $("#Players");
+            this.$playerFormData = $("#playerFormData");
+
+            //Event handlers
+            this.$players.change(function () { parent.addPlayer(); });
+            this.$rankedPlayers.sortable({
+                stop: function() {
+                    parent.onReorder();
+                }
+            });
+        },
+        onReorder: function () {
+            var parent = this;
+
+            this.$rankedPlayersListItem = $("#rankedPlayers li");
+            this.$rankedPlayersListItem.each(function (index, value) {
+                var listItem = $(value);
+                var playerId = listItem.attr("data-playerId");
+                var rank = index + 1;
+                $("#" + playerId).val(rank);
+
+                var playerName = listItem.attr("data-playerName");
+                listItem.html(parent.generatePlayerRankListItemString(playerName, rank));
+            });
+        },
+        generatePlayerRankListItemString: function (playerName, playerRank) {
+            return playerName + " - Rank: " + playerRank;
+        },
+        addPlayer: function () {
+            var selectedOption = this.$players.find(":selected");
+
+            if (selectedOption.text() == "Add A Player") {
+                return alert("You must pick a player.");
+            }
+
+            var playerId = selectedOption.val();
+            var playerName = selectedOption.text();
+            var playerRow = "<input type='hidden' name='PlayerGameResults[" + this._playerIndex +
+                            "].PlayerId' value='" + playerId + "'/>" +
+                            "<input type='hidden' id='" + playerId +
+                            "' name='PlayerGameResults[" + this._playerIndex +
+                            "].GameRank' value='" + this._playerRank + "'/>";
+
+            this.$playerFormData.append(playerRow);
+
+            var playerItem = "<li id='li" + playerId +
+                              "' data-playerId='" + playerId +
+                              "' data-playerName='" + playerName +
+                              "'>" + this.generatePlayerRankListItemString(playerName, this._playerRank) + "</li>";
+
+            this.$rankedPlayers.append(playerItem);
+            this._playerIndex++;
+            this._playerRank++;
+            selectedOption.remove();
+
+            return null;
+        },
+
+        //Properties
+        set_playerIndex: function (value) {
+            this._playerIndex = value;
+        },
+        get_playerIndex: function () {
+            return this._playerIndex;
+        },
+        set_playerRank: function (value) {
+            this._playerRank = value;
+        },
+        get_playerRank: function () {
+            return this._playerRank;
+        }
+    }; //end prototypes
+
+    //Creation
+    $(document).ready(function () {
+        var createPlayedGame = new window.Views.PlayedGame.CreatePlayedGame();
+        createPlayedGame.init();
     });
-}
-
-function generatePlayerRankListItemString(playerName, playerRank) {
-    return playerName + " - Rank: " + playerRank;
-}
-
-function addPlayer() {
-    var selectedOption = $("#Players").find(":selected");
-
-    if (selectedOption.text() == "Add A Player") {
-        return alert("You must pick a player.");
-    }
-    var playerId = selectedOption.val();
-    var playerName = selectedOption.text();
-    var playerRow = "<input type='hidden' name='PlayerGameResults[" + gbl_playerIndex + "].PlayerId' value='" + playerId + "'/>"
-        + "<input type='hidden' id='" + playerId + "' name='PlayerGameResults[" + gbl_playerIndex + "].GameRank' value='" + gbl_playerRank + "'/>";
-    $("#playerFormData").append(playerRow);
-
-    var playerItem = "<li id='li" + playerId + "' data-playerId='" + playerId + "' data-playerName='" + playerName + "'>" + generatePlayerRankListItemString(playerName, gbl_playerRank) + "</li>";
-    $("#rankedPlayers").append(playerItem);
-
-    gbl_playerIndex++;
-    gbl_playerRank++
-    selectedOption.remove();
-}
