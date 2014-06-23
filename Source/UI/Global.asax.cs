c3195d6ab6b449ed88c6cd0e3d3bbc08b1187f25
Jake Gordon <jacob.gordon@Emerson.com>
2014-06-23 13:53:32 -0400

    added support for .map mime type. Had to remove before adding on my local machine.

--- Source/UI/Global.asax.cs
+++ Source/UI/Global.asax.cs
@@ -16,6 +16,9 @@ namespace UI
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        internal const string INFO_MESSAGE_TO_LOG = "This is just an informational message. A request was sent to: {0}";
+        internal const string INFORMATIONAL_TAG = "InfoTag";
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -31,42 +34,88 @@ namespace UI
         protected void Application_Error()
         {
             var exception = Server.GetLastError();
+            LogErrorToRayGun(exception);
+            LogErrorToBugSnag(exception);
+            LogErrorToRollBar(exception);
 
-            //log error in Raygun
-            RaygunClient raygunClient = new RaygunClient();
-            raygunClient.Send(exception);
+        }
 
-            //log error in Bugsnag
-            BugSnag bugSnagClient = new BugSnag("839b07c129a3c860855dda0efe3505fe");
-            bugSnagClient.Notify(exception, "From-Dev");
+        private static void LogErrorToRayGun(Exception exception)
+        {
+            try
+            {
+                RaygunClient raygunClient = new RaygunClient();
+                raygunClient.Send(exception);
+            }
+            catch (Exception) { }
+        }
 
-            //log error in Rollbar
-            RollbarClient rollbarClient = new RollbarClient("ea76150e8b7741ea9433b3052dc861a2");
-            rollbarClient.SendErrorException(exception);
+        private static void LogErrorToBugSnag(Exception exception)
+        {
+            try
+            {
+                BugSnag bugSnagClient = new BugSnag("839b07c129a3c860855dda0efe3505fe");
+                bugSnagClient.Notify(exception, "From-Dev");
+            }
+            catch (Exception) { }
+        }
 
+        private static void LogErrorToRollBar(Exception exception)
+        {
+            try
+            {
+                RollbarClient rollbarClient = new RollbarClient("ea76150e8b7741ea9433b3052dc861a2");
+                rollbarClient.SendErrorException(exception);
+            }
+            catch (Exception) { }
         }
 
         //TODO Remove this when error logger bake-off is over. Just using this to test logging informational messages
         protected void Application_BeginRequest()
         {
-            if(DateTime.Now.Millisecond % 2 == 0)
+            if (DateTime.Now.Millisecond % 2 == 0)
             {
-                string infoTag = "InfoTag";
-                string infoMessageToLog = "This is just an informational message. A request was sent to: " + Request.RawUrl;
-                //log error in Raygun
-                RaygunClient raygunClient = new RaygunClient();
-                raygunClient.SendInBackground(new Exception(infoMessageToLog), new List<string> { infoTag });
-
-                //log error in Bugsnag. Unfortunately getting ArgumentNullException if trying to pass extra data.
-                BugSnag bugSnagClient = new BugSnag("839b07c129a3c860855dda0efe3505fe");
-                bugSnagClient.Notify(new Exception(infoMessageToLog));
-
-                //log error in Rollbar
-                RollbarClient rollbarClient = new RollbarClient("ea76150e8b7741ea9433b3052dc861a2");
-                Dictionary<string, object> customDataDictionary = new Dictionary<string,object>();
-                customDataDictionary.Add("InfoTag", "infoTag");
-                rollbarClient.SendInfoMessage(infoMessageToLog, customDataDictionary);
+                string infoMessage = string.Format(INFO_MESSAGE_TO_LOG, Request.RawUrl);
+                LogRayGunInformationalMessage(infoMessage);
+                LogBugSnagInformationalMessage(infoMessage);
+                LogRollbarInformationalMessage(infoMessage);
             }
         }
+
+        private static void LogRayGunInformationalMessage(string infoMessageToLog)
+        {
+            try
+            {
+                RaygunClient raygunClient = new RaygunClient();
+                raygunClient.SendInBackground(new Exception(infoMessageToLog), new List<string> { INFORMATIONAL_TAG });
+            }
+            catch (Exception) { }
+
+        }
+
+        private static void LogRollbarInformationalMessage(string infoMessageToLog)
+        {
+            try
+            {
+                RollbarClient rollbarClient = new RollbarClient("ea76150e8b7741ea9433b3052dc861a2");
+                Dictionary<string, object> customDataDictionary = new Dictionary<string, object>();
+                customDataDictionary.Add(INFORMATIONAL_TAG, "infoTag");
+                rollbarClient.SendInfoMessage(infoMessageToLog, customDataDictionary);
+            }
+            catch (Exception) { }
+        }
+
+        private static void LogBugSnagInformationalMessage(string infoMessageToLog)
+        {
+            try
+            {
+                //TODO Unfortunately getting ArgumentNullException if trying to pass extra data.
+                BugSnag bugSnagClient = new BugSnag("839b07c129a3c860855dda0efe3505fe");
+                bugSnagClient.Notify(new Exception(infoMessageToLog));
+            }
+            catch (Exception) { }
+        }
+
+
     }
 }
