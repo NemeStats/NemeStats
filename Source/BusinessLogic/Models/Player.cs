144fd57251bb20f0757b5fc3c7b248fcffc06dc5
Jake Gordon <jacob.gordon@Emerson.com>
2014-07-18 17:09:16 -0400

    now has a unique constraint on player for name and gaming group id

--- Source/BusinessLogic/Models/Player.cs
+++ Source/BusinessLogic/Models/Player.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,13 @@ namespace BusinessLogic.Models
 {
     public class Player : EntityWithTechnicalKey
     {
+        [Index("IX_ID_AND_NAME", 1, IsUnique = true)]
         public override int Id { get; set; }
 
         public int GamingGroupId { get; set; }
 
         [StringLength(255)]
+        [Index("IX_ID_AND_NAME", 2, IsUnique = true)]
         public string Name { get; set; }
         public bool Active { get; set; }
 
