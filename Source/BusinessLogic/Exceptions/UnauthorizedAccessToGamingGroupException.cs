c159ec720f82d597c25b17f0b10537acf5a78ed3
Jake Gordon <jacob.gordon@Emerson.com>
2014-07-20 13:19:42 -0400

    Attempt at creating custom exceptions was a failure since I can't create custom constructors to alleviate the effort of putting together the message strings every time. Will back these changes  out shortly.

--- Source/BusinessLogic/Exceptions/UnauthorizedAccessToGamingGroupException.cs
+++ Source/BusinessLogic/Exceptions/UnauthorizedAccessToGamingGroupException.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic.Exceptions
 {
-    public class UnauthorizedAccessToGamingGroupException
+    public class UnauthorizedAccessToGamingGroupException : Exception
     {
+        internal const string EXCEPTION_MESSAGE_USER_DOES_NOT_HAVE_ACCESS_TO_GAMING_GROUP
+          = "User with Id '{0} is unauthorized to access '{1}' with Id '{2}'";
+
+        public UnauthorizedAccessToGamingGroupException(string applicationUserId, string entityName, string entityId)
+            
+        {
+
+        }
+
+        public UnauthorizedAccessToGamingGroupException()
+        {
+        }
+
+        public UnauthorizedAccessToGamingGroupException(string message)
+            : base(message)
+        {
+        }
+        public UnauthorizedAccessToGamingGroupException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
     }
 }
