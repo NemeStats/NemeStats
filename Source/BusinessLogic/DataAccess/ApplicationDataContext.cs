8ee08dd0602a205981f1a20e60694407f891d8a6
Jake Gordon <jacob.gordon@Emerson.com>
2014-07-26 21:02:00 -0400

    some changes were apparently untracked. pushing them up..

---
+++ Source/BusinessLogic/DataAccess/ApplicationDataContext.cs
@@ -0,0 +1,108 @@
+﻿using BusinessLogic.DataAccess.Security;
+using BusinessLogic.Models.User;
+using System;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace BusinessLogic.DataAccess
+{
+    public class ApplicationDataContext : DataContext
+    {
+        internal const string CONNECTION_NAME = "DefaultConnection";
+        internal const string EXCEPTION_MESSAGE_CURRENT_GAMING_GROUP_ID_CANNOT_BE_NULL = "currentUser.CurrentGamingGroupId cannot be null";
+        private SecuredEntityValidatorFactory securedEntityValidatorFactory;
+        private NemeStatsDbContext nemeStatsDbContext;
+
+        //TODO do i really need this constructor? MockRepository.GenerateMock<ApplicationDbContext>() fails saying it needs a parameterless constructor
+        public ApplicationDataContext() 
+            : this(new NemeStatsDbContext(), new SecuredEntityValidatorFactory())
+        {
+
+        }
+
+        public ApplicationDataContext(
+            NemeStatsDbContext nemeStatsDbContext,
+            SecuredEntityValidatorFactory securedEntityValidatorFactory)
+        {
+            this.nemeStatsDbContext = nemeStatsDbContext;
+            this.securedEntityValidatorFactory = securedEntityValidatorFactory;
+        }
+
+        public virtual void CommitAllChanges()
+        {
+            nemeStatsDbContext.SaveChanges();
+        }
+
+        public virtual IQueryable<TEntity> GetQueryable<TEntity>() where TEntity : class
+        {
+            return nemeStatsDbContext.Set<TEntity>();
+        }
+
+        internal virtual TEntity AddOrInsertOverride<TEntity>(TEntity entity) where TEntity : class
+        {
+            nemeStatsDbContext.Set<TEntity>().AddOrUpdate(entity);
+
+            return entity;
+        }
+        //TODO If the passed in TEntity that is new, the Id will not be set until SaveChanges is called
+        public virtual TEntity Save<TEntity>(TEntity entity, ApplicationUser currentUser) where TEntity : EntityWithTechnicalKey
+        {
+            ValidateArguments<TEntity>(entity, currentUser);
+
+            if (entity.AlreadyInDatabase())
+            {
+                //TODO update comments to indicate it can throw an exception
+                SecuredEntityValidator<TEntity> validator = securedEntityValidatorFactory.MakeSecuredEntityValidator<TEntity>();
+                validator.ValidateAccess(entity, currentUser, typeof(TEntity));
+            }
+            else
+            {
+                SetGamingGroupIdIfEntityIsSecured<TEntity>(entity, currentUser);
+            }
+
+            TEntity savedEntity = AddOrInsertOverride<TEntity>(entity);
+            CommitAllChanges();
+
+            return savedEntity;
+        }
+
+        private static void ValidateArguments<TEntity>(TEntity entity, ApplicationUser currentUser) where TEntity : EntityWithTechnicalKey
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            if (currentUser == null)
+            {
+                throw new ArgumentNullException("currentUser");
+            }
+        }
+
+        private static void SetGamingGroupIdIfEntityIsSecured<TEntity>(TEntity entity, ApplicationUser currentUser) where TEntity : EntityWithTechnicalKey
+        {
+            if (typeof(SecuredEntityWithTechnicalKey).IsAssignableFrom(typeof(TEntity)))
+            {
+                if(currentUser.CurrentGamingGroupId == null)
+                {
+                    throw new ArgumentException(EXCEPTION_MESSAGE_CURRENT_GAMING_GROUP_ID_CANNOT_BE_NULL);
+                }
+                SecuredEntityWithTechnicalKey securedEntity = entity as SecuredEntityWithTechnicalKey;
+                securedEntity.GamingGroupId = currentUser.CurrentGamingGroupId.Value;
+            }
+        }
+
+        public void Delete<TEntity>(TEntity entity, ApplicationUser currentUser) where TEntity : EntityWithTechnicalKey
+        {
+            SecuredEntityValidator<TEntity> validator = securedEntityValidatorFactory.MakeSecuredEntityValidator<TEntity>();
+            validator.ValidateAccess(entity, currentUser, typeof(TEntity));
+            nemeStatsDbContext.Set<TEntity>().Remove(entity);
+            CommitAllChanges();
+        }
+
+        public void Dispose()
+        {
+            nemeStatsDbContext.Dispose();
+        }
+    }
+}
---
+++ Tests/BusinessLogic.Tests/UnitTests/DataAccessTests/EntityWithTechnicalKeyTests/AlreadyInDatabaseTests.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLogic.DataAccess;
+using BusinessLogic.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests.UnitTests.DataAccessTests.EntityWithTechnicalKeyTests
+{
+    [TestFixture]
+    public class AlreadyInDatabaseTests
+    {
+        [Test]
+        public void ItReturnsFalseIfTheIdIsNull()
+        {
+            EntityWithStringKey entityWithStringKey = new EntityWithStringKey();
+
+            Assert.False(entityWithStringKey.AlreadyInDatabase());
+        }
+
+        [Test]
+        public void ItReturnsFalseIfTheIdIsNotSetForAnIntKey()
+        {
+            EntityWithIntKey entityWithIntKey = new EntityWithIntKey();
+
+            Assert.False(entityWithIntKey.AlreadyInDatabase());
+        }
+
+        [Test]
+        public void ItReturnsFalseIfTheIdIsNotSetForAGuidKey()
+        {
+            EntityWithGuidKey entityWithGuidKey = new EntityWithGuidKey();
+
+            Assert.False(entityWithGuidKey.AlreadyInDatabase());
+        }
+
+        private class EntityWithGuidKey : EntityWithTechnicalKey<Guid>
+        {
+            public override Guid Id { get; set; }
+        }
+
+        private class EntityWithStringKey : EntityWithTechnicalKey<string>
+        {
+            public override string Id { get; set; }
+        }
+
+        private class EntityWithIntKey : EntityWithTechnicalKey<int>
+        {
+            public override int Id { get; set; }
+        }
+    }
+}
