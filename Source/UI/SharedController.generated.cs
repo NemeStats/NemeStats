eca8737baaf5d332d0e1ba1d0bc21939d55b52da
Tosho Toshev <tosho_az@yahoo.com>
2014-08-27 23:04:14 -0400

    The home page is re-styled. A new layout is introduced. All views are put under a different layout

--- Source/UI/SharedController.generated.cs
+++ Source/UI/SharedController.generated.cs
@@ -36,10 +36,12 @@ namespace T4MVC
             public _ViewNamesClass ViewNames { get { return s_ViewNames; } }
             public class _ViewNamesClass
             {
+                public readonly string _BaseLayout = "_BaseLayout";
                 public readonly string _Layout = "_Layout";
                 public readonly string _LoginPartial = "_LoginPartial";
                 public readonly string Error = "Error";
             }
+            public readonly string _BaseLayout = "~/Views/Shared/_BaseLayout.cshtml";
             public readonly string _Layout = "~/Views/Shared/_Layout.cshtml";
             public readonly string _LoginPartial = "~/Views/Shared/_LoginPartial.cshtml";
             public readonly string Error = "~/Views/Shared/Error.cshtml";
