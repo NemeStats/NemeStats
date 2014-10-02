7578859687294dbd94887cc7c72aa83c8f242759
Jake Gordon <jacob.gordon@Emerson.com>
2014-10-01 21:35:34 -0400

    tosho going to help debug

--- Source/UI/App_Start/IdentityConfig.cs
+++ Source/UI/App_Start/IdentityConfig.cs
@@ -12,6 +12,9 @@ using System.Threading.Tasks;
 using System.Web;
 using BusinessLogic.Models.User;
 using BusinessLogic.DataAccess;
+using System.Net;
+using SendGrid;
+using System.Configuration;
 
 namespace IdentitySample.Models
 {
@@ -91,8 +94,36 @@ namespace IdentitySample.Models
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            return configSendGridasync(message);
+        }
+
+        private Task configSendGridasync(IdentityMessage message)
+        {
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new System.Net.Mail.MailAddress(
+                                "nemestats@gmail.com", "NemeStats");
+            myMessage.Subject = message.Subject;
+            myMessage.Text = message.Body;
+            myMessage.Html = message.Body;
+
+            var credentials = new NetworkCredential(
+                       ConfigurationManager.AppSettings["emailServiceUserName"],
+                       ConfigurationManager.AppSettings["emailServicePassword"]
+                       );
+
+            // Create a Web transport for sending email.
+            var transportWeb = new Web(credentials);
+
+            // Send the email.
+            if (transportWeb != null)
+            {
+                return transportWeb.DeliverAsync(myMessage);
+            }
+            else
+            {
+                return Task.FromResult(0);
+            }
         }
     }
 
