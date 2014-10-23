d061ec4510bc2be17222a594a5fa1e17d1ce5b3b
Mike Koch <mkoch227@gmail.com>
2014-10-22 21:18:19 -0400

    ApplicationUserManager now has same password security set as the model's requirements

--- Source/BusinessLogic/Logic/Users/ApplicationUserManager.cs
+++ Source/BusinessLogic/Logic/Users/ApplicationUserManager.cs
@@ -23,10 +23,10 @@ namespace BusinessLogic.Logic.Users
             this.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
             // Configure user lockout defaults
             this.UserLockoutEnabledByDefault = true;
