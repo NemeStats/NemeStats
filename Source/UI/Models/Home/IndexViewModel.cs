7d3e4f3087487205cb657fc4b46f20776402d3fc
Jake Gordon <jacob.gordon@Emerson.com>
2014-10-28 22:23:39 -0400

    stubbed out UI for recent nemesis changes.

--- Source/UI/Models/Home/IndexViewModel.cs
+++ Source/UI/Models/Home/IndexViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using BusinessLogic.Models.Games;
 using System.Collections.Generic;
 using System.Linq;
+using UI.Models.Nemeses;
 using UI.Models.Players;
 
 namespace UI.Models.Home
 {
     public class HomeIndexViewModel
     {
+        public HomeIndexViewModel()
+        {
+            TopPlayers = new List<TopPlayerViewModel>();
+            RecentPublicGames = new List<PublicGameSummary>();
+            RecentNemesisChanges = new List<NemesisChangeViewModel>();
+        }
         public List<TopPlayerViewModel> TopPlayers { get; set; }
         public List<PublicGameSummary> RecentPublicGames { get; set; }
+        public List<NemesisChangeViewModel> RecentNemesisChanges { get; set; }
     }
 }
---
+++ Source/UI/Models/Nemeses/NemesisChangeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Nemeses
+{
+    public class NemesisChangeViewModel
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public int NemesisPlayerId { get; set; }
+        public string NemesisPlayerName { get; set; }
+        public int LossPercentageVersusNemesis { get; set; }
+    }
+}
