29200d1ee1f0d9e6004992d94eb4f240a1865b14
Tosho Toshev <tosho_az@yahoo.com>
2014-11-09 13:26:11 -0500

    #54 - The ability to change the gaming group name is added to Nemestats.

---
+++ Source/UI/Scripts/GamingGroup/gamingGroup.js
@@ -0,0 +1,34 @@
+﻿//Usings
+Namespace("Views.GamingGroup");
+
+//Initialization
+Views.GamingGroup.GamingGroupView = function () {
+    this.$container = null;
+    this.$title = null;
+    this._serviceAddress = "/GamingGroup/Edit";
+
+};
+
+//Implementation
+Views.GamingGroup.GamingGroupView.prototype = {
+    init: function () {
+        var parent = this;
+        this.$title = $("#gamingGroupTitle");
+        this.$title.toEditBox({ onFocusOut: $.proxy(parent.renameGamingGroup, this) });
+    },
+    renameGamingGroup: function (element) {
+        var parent = this;
+        $.ajax({
+            type: "POST",
+            url: parent._serviceAddress,
+            data: { "gamingGroupName": element.value },
+            success: function (data) {
+
+            },
+            error: function (err) {
+                alert("Error " + err.status + ":\r\n" + err.statusText);
+            },
+            dataType: "json"
+        });
+    }
+}
