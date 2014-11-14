18cd52a8f70930dec04707d1a7c5fd942859cc14
Jake Gordon <jacob.gordon@Emerson.com>
2014-11-13 21:33:07 -0500

    No longer have to have a winner. Shows TEAM LOSS on home page for winner.

--- Source/BusinessLogic/Models/Games/Validation/PlayerRankValidator.cs
+++ Source/BusinessLogic/Models/Games/Validation/PlayerRankValidator.cs
@@ -10,7 +10,6 @@ namespace BusinessLogic.Models.Games.Validation
         internal const string EXCEPTION_MESSAGE_EACH_PLAYER_RANK_MUST_HAVE_A_PLAYER_ID = "Each PlayerRank must have a valid PlayerId.";
         internal const string EXCEPTION_MESSAGE_EACH_PLAYER_RANK_MUST_HAVE_A_GAME_RANK = "Each PlayerRank must have a valid GameRank.";
         internal const string EXCEPTION_MESSAGE_MUST_PASS_AT_LEAST_TWO_PLAYERS = "Must pass in at least two players.";
-        internal const string EXCEPTION_MESSAGE_GAME_MUST_HAVE_A_WINNER = "The game must have at least one winner (GameRank = 1).";
         internal const string EXCEPTION_MESSAGE_NO_PLAYER_CAN_HAVE_A_HIGHER_RANK_THAN_THE_NUMBER_OF_PLAYERS = "No player can have a higher rank than the total number of players in the game.";
 
         public static void ValidatePlayerRanks(List<PlayerRank> playerRanks)
@@ -19,7 +18,6 @@ namespace BusinessLogic.Models.Games.Validation
             ValidateThatAllPlayerIdsAreSet(playerRanks);
             ValidateThatAllGameRanksAreSet(playerRanks);
             ValidateThatThereAreAtLeastTwoPlayers(playerRanks);
-            ValidateThatThereIsAWinner(playerRanks);
             ValidateThatNoPlayerHasARankGreaterThanTheNumberOfPlayers(playerRanks);
             ValidateNoPlayerHasARankLessThanOne(playerRanks);
         }
@@ -61,14 +59,6 @@ namespace BusinessLogic.Models.Games.Validation
             if (playerRanks.Count < 2)
             {
                 throw new ArgumentException(EXCEPTION_MESSAGE_MUST_PASS_AT_LEAST_TWO_PLAYERS);
-            }
-        }
-
-        private static void ValidateThatThereIsAWinner(List<PlayerRank> playerRanks)
-        {
-            if (playerRanks.All(playerRank => playerRank.GameRank != 1))
-            {
-                throw new ArgumentException(EXCEPTION_MESSAGE_GAME_MUST_HAVE_A_WINNER);
             }
         }
 
