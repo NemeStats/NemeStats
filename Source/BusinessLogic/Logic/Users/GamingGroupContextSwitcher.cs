bb03c16228fe155c9e6cfa38242b48caa451260d
Jake Gordon <jacob.gordon@Emerson.com>
2014-11-26 08:43:11 -0500

    finished the switch gaming group logic.

--- Source/BusinessLogic/Logic/Users/GamingGroupContextSwitcher.cs
+++ Source/BusinessLogic/Logic/Users/GamingGroupContextSwitcher.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic.DataAccess;
 using BusinessLogic.Models.User;
 
 namespace BusinessLogic.Logic.Users
 {
     public class GamingGroupContextSwitcher : IGamingGroupContextSwitcher
     {
+        public const string EXCEPTION_MESSAGE_NO_ACCESS = "User with Id '{0}' does not have access to GamingGroup with Id '{1}'";
+
+        private readonly IDataContext dataContext;
+
+        public GamingGroupContextSwitcher(IDataContext dataContextMock)
+        {
+            this.dataContext = dataContextMock;
+        }
+
         public void SwitchGamingGroupContext(int gamingGroupId, ApplicationUser currentUser)
         {
-            
+            if (gamingGroupId == currentUser.CurrentGamingGroupId.Value)
+            {
+                return;
+            }
+
+            bool hasAccess  = dataContext.GetQueryable<UserGamingGroup>()
+                                         .Any(userGamingGroup => userGamingGroup.ApplicationUserId == currentUser.Id
+                                                                 && userGamingGroup.GamingGroupId == gamingGroupId);
+
+            if (!hasAccess)
+            {
+                throw new UnauthorizedAccessException(string.Format(EXCEPTION_MESSAGE_NO_ACCESS, currentUser.Id, gamingGroupId));
+            }
+
+            var user = dataContext.FindById<ApplicationUser>(currentUser.Id);
+            user.CurrentGamingGroupId = gamingGroupId;
+            dataContext.Save(user, currentUser);
         }
     }
 }
--- Tests/BusinessLogic.Tests/UnitTests/LogicTests/UsersTests/GamingGroupContextSwitcherTests/SwitchGamingGroupContextTests.cs
+++ Tests/BusinessLogic.Tests/UnitTests/LogicTests/UsersTests/GamingGroupContextSwitcherTests/SwitchGamingGroupContextTests.cs
@@ -3,11 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic.DataAccess;
+using BusinessLogic.Logic.Users;
+using BusinessLogic.Models.User;
+using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace BusinessLogic.Tests.UnitTests.LogicTests.UsersTests.GamingGroupContextSwitcherTests
 {
     public class SwitchGamingGroupContextTests
     {
+        protected IDataContext dataContextMock;
+        protected GamingGroupContextSwitcher contextSwitcher;
+        protected ApplicationUser currentUser;
+        protected ApplicationUser retrievedUser;
+        protected List<UserGamingGroup> userGamingGroups;
+        protected int gamingGroupIdUserCanSee = 1;
+        protected int gamingGroupidUserCannotSee = 2;
+            
+        [SetUp]
+        public void SetUp()
+        {
+            dataContextMock = MockRepository.GenerateMock<IDataContext>();
+            contextSwitcher = new GamingGroupContextSwitcher(dataContextMock);
 
+            currentUser = new ApplicationUser
+            {
+                Id = "user id",
+                CurrentGamingGroupId = 777
+            };
+            retrievedUser = new ApplicationUser
+            {
+                Id = "user id"
+            };
+
+            userGamingGroups = new List<UserGamingGroup>
+            {
+                new UserGamingGroup
+                {
+                    Id = 1,
+                    ApplicationUserId = currentUser.Id,
+                    GamingGroupId = gamingGroupIdUserCanSee
+                },
+                new UserGamingGroup
+                {
+                    Id = 2,
+                    ApplicationUserId = "some other id the user cant see",
+                    GamingGroupId = gamingGroupIdUserCanSee
+                }
+            };
+
+            dataContextMock.Expect(mock => mock.GetQueryable<UserGamingGroup>())
+                           .Return(userGamingGroups.AsQueryable());
+            dataContextMock.Expect(mock => mock.FindById<ApplicationUser>(currentUser.Id))
+                           .Return(retrievedUser);
+        }
+
+        [Test]
+        public void ItValidatesThatTheUserHasAccessToThisGamingGroup()
+        {
+            int gamingGroupTheUserCantSee = -999;
+            string expectedMessage = string.Format(GamingGroupContextSwitcher.EXCEPTION_MESSAGE_NO_ACCESS, currentUser.Id, gamingGroupTheUserCantSee);
+
+            var exception = Assert.Throws<UnauthorizedAccessException>(() => contextSwitcher.SwitchGamingGroupContext(gamingGroupTheUserCantSee, currentUser));
+
+            Assert.That(exception.Message, Is.EqualTo(expectedMessage));
+        }
+
+        [Test]
+        public void ItDoesNotSaveIfTheUserIsAlreadySetToThatGamingGroup()
+        {
+            contextSwitcher.SwitchGamingGroupContext(currentUser.CurrentGamingGroupId.Value, currentUser);
+
+            dataContextMock.AssertWasNotCalled(mock => mock.Save(Arg<ApplicationUser>.Is.Anything, Arg<ApplicationUser>.Is.Anything));
+        }
+
+        [Test]
+        public void ItUpdatesTheUsersGamingGroup()
+        {
+            contextSwitcher.SwitchGamingGroupContext(gamingGroupIdUserCanSee, currentUser);
+
+            dataContextMock.AssertWasCalled(mock => mock.Save(
+                Arg<ApplicationUser>.Matches(user => user.CurrentGamingGroupId == gamingGroupIdUserCanSee && user.Id == currentUser.Id), 
+                Arg<ApplicationUser>.Is.Same(currentUser)));
+        }
     }
 }
