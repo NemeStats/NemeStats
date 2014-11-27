5342ee7dee46c7993ad11f1243a6f902af263889
Tosho Toshev <tosho_az@yahoo.com>
2014-11-27 11:51:36 -0500

    90% of the gaming group UI is done

--- Source/UI/App_Start/BundleConfig.cs
+++ Source/UI/App_Start/BundleConfig.cs
@@ -7,8 +7,9 @@ namespace UI
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.cookie.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- Source/UI/Scripts/Shared/_LoginPartial.js
+++ Source/UI/Scripts/Shared/_LoginPartial.js
@@ -3,13 +3,58 @@ Namespace("Views.Shared");
 
 //Initialization
 Views.Shared.LoginPartial = function () {
-    
+    this.$gamingGroupSwitcher = null;
+    this.$currentGamingGroup = null;
+    this._cookieName = "gamingGroupsCookie";
+    this._getGamingGroupsService = "/GamingGroup/GetUsersGamingGroups";
 };
 
 //Implementation
 Views.Shared.LoginPartial.prototype = {
     init: function () {
-        alert("Tosho");
+        this.$gamingGroupSwitcher = $("#gamingGroupSwitcher");
+        this.$currentGamingGroup = $("#currentGamingGroup");
+
+        if (!$.cookie(this._cookieName)) {
+            this.getGamingGroups();
+        } else {
+            this.setGamingGroups(JSON.parse($.cookie(this._cookieName)));
+        }
+        
+    },
+    getGamingGroups: function () {
+        var parent = this;
+        $.ajax({
+            type: "POST",
+            url: parent._getGamingGroupsService,
+            async: false,
+            success: function (data) {
+                $.cookie(parent._cookieName, JSON.stringify(data), { expires: 1, path: '/' });
+            },
+            error: function (err) {
+                console.log("Error " + err.status + ":\r\n" + err.statusText);
+            },
+            dataType: "json"
+        });
+    },
+    setGamingGroups: function (gamingGroups) {
+        var parent = this;
+        this.$gamingGroupSwitcher.empty();
+
+        for (var i = 0; i < gamingGroups.length; i++) {
+            var gamingGroup = gamingGroups[i];
+            if (gamingGroup.IsCurrentGamingGroup == true) {
+                this.$currentGamingGroup.html(gamingGroup.Name);
+            }
+            var gamingGroupItem = "<li><a href=# class='gamingGroupItem' data='"+ gamingGroup.Id +"'>"+ gamingGroup.Name +"</a></li>";
+            this.$gamingGroupSwitcher.append(gamingGroupItem);
+            $(".gamingGroupItem").off('click').on('click', function () {
+                parent.switchGamingGroup(this);
+            });
+        }
+    },
+    switchGamingGroup: function (gamingGroupItem) {
+        alert("todo - call the switch ajax");
     }
 };
 
