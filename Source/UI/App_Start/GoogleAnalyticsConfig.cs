8dd6197d97584cfbe647eb2734f3dfc552d99dba
Jake Gordon <jacob.gordon@Emerson.com>
2014-12-03 22:00:14 -0500

    universal analytics tracking id now comes from the config.

---
+++ Source/UI/App_Start/GoogleAnalyticsConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Abstractions;
+using System.Linq;
+using System.Web;
+
+namespace UI.App_Start
+{
+    public class GoogleAnalyticsConfig
+    {
+        public const string UNIVERSAL_ANALYTICS_TRACKING_ID_APP_KEY = "UniversalAnalytics.TrackingId";
+
+        private static readonly object syncRoot = new Object();
+        private static string googleAnalyticsTrackingCode;
+
+        public static string GetGoogleAnalyticsTrackingId()
+        {
+            if (googleAnalyticsTrackingCode == null)
+            {
+                lock (syncRoot)
+                {
+                    if (googleAnalyticsTrackingCode == null)
+                    {
+                        ConfigurationManager configManager = new ConfigurationManager();
+                        googleAnalyticsTrackingCode = configManager.AppSettings[UNIVERSAL_ANALYTICS_TRACKING_ID_APP_KEY];
+                    }
+                }
+            }
+
+            return googleAnalyticsTrackingCode;
+        }
+    }
+}
