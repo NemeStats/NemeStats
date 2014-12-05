eba2a862660b00a070cf4738edca6890ee7cfec5
Mike Koch <mkoch227@gmail.com>
2014-12-04 22:48:13 -0500

    Migration to recalculate champions
    now that there is no minimum game requirement

---
+++ Source/BusinessLogic/Migrations/201412050347345_RecalculateChampions.cs
@@ -0,0 +1,30 @@
+using BusinessLogic.DataAccess;
+using BusinessLogic.DataAccess.Repositories;
+using BusinessLogic.Logic.Champions;
+
+namespace BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RecalculateChampions : DbMigration
+    {
+        public override void Up()
+        {
+            using (NemeStatsDbContext dbContext = new NemeStatsDbContext())
+            {
+                using (NemeStatsDataContext dataContext = new NemeStatsDataContext())
+                {
+                    IChampionRepository championRepository = new ChampionRepository(dataContext);
+
+                    IChampionRecalculator championRecalculator = new ChampionRecalculator(dataContext, championRepository);
+                    championRecalculator.RecalculateAllChampions();
+                }
+            }
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
