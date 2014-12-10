1b4a9a6ea55a3626553c7c26dd0c8b70e54de886
Jake Gordon <jacob.gordon@Emerson.com>
2014-12-10 15:38:58 -0500

    added an integration test for recalculating champions. Also going to try recalculating in production one more time.

---
+++ Source/BusinessLogic/Migrations/201412102038100_RecalculateChampionsYetAgain.cs
@@ -0,0 +1,33 @@
+using BusinessLogic.DataAccess;
+using BusinessLogic.DataAccess.Repositories;
+using BusinessLogic.DataAccess.Security;
+using BusinessLogic.Logic.Champions;
+
+namespace BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RecalculateChampionsYetAgain : DbMigration
+    {
+        public override void Up()
+        {
+            using (NemeStatsDbContext dbContext = new NemeStatsDbContext())
+            {
+                SecuredEntityValidatorFactory factory = new SecuredEntityValidatorFactory();
+
+                using (NemeStatsDataContext dataContext = new NemeStatsDataContext(dbContext, factory))
+                {
+                    IChampionRepository championRepository = new ChampionRepository(dataContext);
+
+                    IChampionRecalculator championRecalculator = new ChampionRecalculator(dataContext, championRepository);
+                    championRecalculator.RecalculateAllChampions();
+                }
+            }
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Tests/BusinessLogic.Tests/IntegrationTests/LogicTests/ChampionsTests/RecalculateChampionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.DataAccess;
+using BusinessLogic.DataAccess.Repositories;
+using BusinessLogic.DataAccess.Security;
+using BusinessLogic.Logic.Champions;
+using BusinessLogic.Models.User;
+using NUnit.Framework;
+
+namespace BusinessLogic.Tests.IntegrationTests.LogicTests.ChampionsTests
+{
+    [TestFixture]
+    public class RecalculateChampionTests
+    {
+        [Test, Ignore("Integration tests")]
+        public void RecalculateForSingleGame()
+        {
+            using (NemeStatsDbContext dbContext = new NemeStatsDbContext())
+            {
+                SecuredEntityValidatorFactory factory = new SecuredEntityValidatorFactory();
+
+                using (NemeStatsDataContext dataContext = new NemeStatsDataContext(dbContext, factory))
+                {
+                    IChampionRepository championRepository = new ChampionRepository(dataContext);
+
+                    IChampionRecalculator championRecalculator = new ChampionRecalculator(dataContext, championRepository);
+                    ApplicationUser user = new ApplicationUser
+                    {
+                        Id = "80629c07-b8df-4deb-a9e3-5b503ce7d7df",
+                        CurrentGamingGroupId = 1
+                    };
+                    championRecalculator.RecalculateChampion(2005, user);
+                }
+            }
+        }
+    }
+}
