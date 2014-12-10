b280435cc54860e875594c07bc03add826385f72
Jake Gordon <jacob.gordon@Emerson.com>
2014-12-10 15:16:33 -0500

    added robots.txt.

--- SQL/coop_games_activity.sql
+++ SQL/coop_games_activity.sql
@@ -1,7 +1,7 @@
 USE [NerdScorekeeper]
 GO
 
-SELECT GameRank, gameDefinition.Name, Playedgame.Id
+SELECT DISTINCT GameRank, gameDefinition.Name, Playedgame.Id
   FROM [dbo].[PlayerGameResult] INNER JOIN PlayedGame on PlayerGameResult.PlayedGameId = PlayedGame.Id
   INNER JOIN GameDefinition ON GameDefinition.Id = PlayedGame.gameDefinitionId
   WHERE gameDefinition.Name LIKE 'Dead Of%' OR GameDefinition.Name LIKE 'Pand%'
