a384799fe9ddcc0bdd1d97d37949e7cc96c8026d
Jake Gordon <jacob.gordon@Emerson.com>
2015-01-31 22:03:32 -0500

    added an expectation to make Dennis's first test pass.

--- Tests/UI.Tests/UnitTests/ControllerTests/GamingGroupControllerTests/GetTopGamingGroupsTests.cs
+++ Tests/UI.Tests/UnitTests/ControllerTests/GamingGroupControllerTests/GetTopGamingGroupsTests.cs
@@ -1,17 +1,27 @@
-﻿using NUnit.Framework;
+﻿using BusinessLogic.Models.GamingGroups;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Rhino.Mocks;
 
 namespace UI.Tests.UnitTests.ControllerTests.GamingGroupControllerTests
 {
 	[TestFixture]
 	public class GetTopGamingGroupsTests : GamingGroupControllerTestBase
 	{
-		[Test]
+        [SetUp]
+	    public override void SetUp()
+	    {
+	        base.SetUp();
+
+            gamingGroupRetrieverMock.Expect(mock => mock.GetTopGamingGroups(Arg<int>.Is.Anything)).Return(new List<TopGamingGroupSummary>());
+	    }
+
+	    [Test]
 		public void ItReturnsTopGamingGroupsView()
 		{
 			var viewResult = gamingGroupControllerPartialMock.GetTopGamingGroups() as ViewResult;
