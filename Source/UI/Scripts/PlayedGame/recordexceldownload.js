b8f46444dbbeccc02995e145d33fc3a9663462c7
Jake Gordon <jacob.gordon@Emerson.com>
2015-02-27 23:44:42 -0500

    added tracking of excel download clicks

---
+++ Source/UI/Scripts/PlayedGame/recordexceldownload.js
@@ -0,0 +1,24 @@
+﻿//Usings
+Namespace("Views._PlayedGamesPartial");
+
+//Initialization
+Views._PlayedGamesPartial.EventTracking = function () {
+    this._googleAnalytics = null;
+    this.$excelDownloadAnchor = null;
+};
+
+//Implementation
+Views._PlayedGamesPartial.EventTracking.prototype = {
+    
+    //Method definitions
+    init: function (gaObject) {
+        var parent = this;
+        this._googleAnalytics = gaObject;
+        this.$excelDownloadAnchor = $("#playedGamesExcelDownload");
+
+        //Event handlers
+        this.$excelDownloadAnchor.on("click", function() {
+            parent._googleAnalytics.trackGAEvent("PlayedGames", "ExcelFileExportClicked", "FromPlayedGamesPartial");
+        });
+    }
+}; //end prototypes
