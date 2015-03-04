1af2037f9e180eabf883bbb68dd90de4fc0f7ca9
Jake Gordon <jacob.gordon@Emerson.com>
2015-03-03 21:45:18 -0500

    added license region to each code file

--- Source/UI/Filters/InjectingActionInvoker.cs
+++ Source/UI/Filters/InjectingActionInvoker.cs
@@ -1,46 +1,17 @@
-﻿//using StructureMap;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-
-//namespace UI.Filters
-//{
-//    //courtesy of Jimmy Bogard: http://lostechies.com/jimmybogard/2010/05/03/dependency-injection-in-asp-net-mvc-filters/
-//    public class InjectingActionInvoker : ControllerActionInvoker
-//    {
-//        private readonly IContainer _container;
-
-//        public InjectingActionInvoker(IContainer container)
-//        {
-//            _container = container;
-//        }
-
-//        protected override FilterInfo GetFilters(
-//            ControllerContext controllerContext,
-//            ActionDescriptor actionDescriptor)
-//        {
-//            var info = base.GetFilters(controllerContext, actionDescriptor);
-
-//            foreach(IAuthorizationFilter filter in info.AuthenticationFilters)
-//            {
-//                _container.BuildUp(filter);
-//            }
-//            foreach (IAuthorizationFilter filter in info.ActionFilters)
-//            {
-//                _container.BuildUp(filter);
-//            }
-//            foreach (IAuthorizationFilter filter in info.ResultFilters)
-//            {
-//                _container.BuildUp(filter);
-//            }
-//            foreach (IAuthorizationFilter filter in info.ExceptionFilters)
-//            {
-//                _container.BuildUp(filter);
-//            }
-
-//            return info;
-//        }
-//    }
-//}
+﻿#region LICENSE
+// NemeStats is a free website for tracking the results of board games.
+//     Copyright (C) 2015 Jacob Gordon
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>
+#endregion
