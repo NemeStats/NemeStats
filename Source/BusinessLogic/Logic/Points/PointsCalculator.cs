f4c87347c468f10909bd5330bd080a3a2b691762
Jake Gordon <jacob.gordon@Emerson.com>
2015-03-19 10:57:34 -0400

    updated points calculator

--- Source/BusinessLogic/Logic/Points/PointsCalculator.cs
+++ Source/BusinessLogic/Logic/Points/PointsCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,20 +10,105 @@ namespace BusinessLogic.Logic.Points
 {
     public class PointsCalculator
     {
+        public static readonly Dictionary<int, int> FIBONACCI_N_PLUS_2 = new Dictionary<int, int>
+        {
+            {1, 1},
+            {2, 2},
+            {3, 3},
+            {4, 5},
+            {5, 8},
+            {6, 13},
+            {7, 21},
+            {8, 34},
+            {9, 55},
+            {10, 89},
+            {11, 144},
+            {12, 233},
+            {13, 377},
+            {14, 610},
+            {15, 987}
+        };
+
         internal const int DEFAULT_POINTS_PER_PLAYER_WHEN_EVERYONE_LOSES = 2;
+        internal const int POINTS_PER_PLAYER = 10;
+
         internal static Dictionary<int, int> CalculatePoints(IList<PlayerRank> playerRanks)
         {
             Dictionary<int, int> playerToPoints = new Dictionary<int, int>(playerRanks.Count);
 
-            if (playerRanks.All(x => x.GameRank != 1))
+            if (EveryoneLost(playerRanks))
             {
-                foreach (var playerRank in playerRanks)
-                {
-                    playerToPoints.Add(playerRank.PlayerId, DEFAULT_POINTS_PER_PLAYER_WHEN_EVERYONE_LOSES);
-                }
+                GiveEveryoneTwoPoints(playerRanks, playerToPoints);
+            }
+            else
+            {
+                CalculatePointsForPlayers(playerRanks, playerToPoints);
             }
 
             return playerToPoints;
         }
+
+        private static void CalculatePointsForPlayers(IList<PlayerRank> playerRanks, Dictionary<int, int> playerToPoints)
+        {
+            int totalNumberOfPlayers = playerRanks.Count;
+            int totalPointsToAllocate = totalNumberOfPlayers * POINTS_PER_PLAYER;
+            decimal denominator = FibonacciSum(totalNumberOfPlayers);
+
+            const int FIBONACCI_OFFSET = 1;
+            int highestRankSlotNotConsumed = 1;
+            
+            for (int rank = 1; rank <= totalNumberOfPlayers; rank++)
+            {
+                List<PlayerRank> playersWithThisRank = playerRanks.Where(x => x.GameRank == rank).ToList();
+                int numberOfPlayersWithThisRank = playersWithThisRank.Count;
+                if (numberOfPlayersWithThisRank > 0)
+                {
+                    int finoacciEndIndex = totalNumberOfPlayers + FIBONACCI_OFFSET - highestRankSlotNotConsumed;
+                    int fibonacciStartIndex = finoacciEndIndex + FIBONACCI_OFFSET - numberOfPlayersWithThisRank;
+                    decimal numerator = FibonacciSum(fibonacciStartIndex, finoacciEndIndex) / numberOfPlayersWithThisRank;
+                    int points = RoundUpIfNonNegligible(totalPointsToAllocate * (numerator / denominator));
+
+                    foreach (var playerRank in playersWithThisRank)
+                    {
+                        playerToPoints.Add(playerRank.PlayerId, points);
+                    }
+                }
+
+                highestRankSlotNotConsumed += numberOfPlayersWithThisRank;
+            }
+        }
+
+        private static void GiveEveryoneTwoPoints(IList<PlayerRank> playerRanks, Dictionary<int, int> playerToPoints)
+        {
+            foreach (var playerRank in playerRanks)
+            {
+                playerToPoints.Add(playerRank.PlayerId, DEFAULT_POINTS_PER_PLAYER_WHEN_EVERYONE_LOSES);
+            }
+        }
+
+        private static bool EveryoneLost(IList<PlayerRank> playerRanks)
+        {
+            return playerRanks.All(x => x.GameRank != 1);
+        }
+
+        private static decimal FibonacciSum(int fibonacciStartIndex, int finoacciEndIndex)
+        {
+            return FIBONACCI_N_PLUS_2.Where(fibonacci => fibonacci.Key >= fibonacciStartIndex 
+                && fibonacci.Key <= finoacciEndIndex).Sum(x => x.Value);
+        }
+
+        private static decimal FibonacciSum(int endIndex)
+        {
+            return FibonacciSum(1, endIndex);
+        }
+
+        private static int RoundUpIfNonNegligible(decimal valueToRound)
+        {
+            decimal floor = Math.Floor(valueToRound);
+            return (int)((valueToRound - floor) > (decimal)0.1 ? floor + 1 : floor);
+        }
+
+        //10 * (numberOfPlayers) * (fibonacciOf(numberOfPlayers + 1 - r) / fibonacciSumFrom(2, numberOfPlayers))
+        //still need to make sure that ranked bucket groupings work correctly.
     }
 }
--- Tests/BusinessLogic.Tests/UnitTests/LogicTests/PointsTests/PointsCalculatorTests.cs
+++ Tests/BusinessLogic.Tests/UnitTests/LogicTests/PointsTests/PointsCalculatorTests.cs
@@ -13,6 +13,13 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PointsTests
     [TestFixture]
     public class PointsCalculatorTests
     {
+        private const int FIRST_PLACE = 1;
+        private const int SECOND_PLACE = 2;
+        private const int THIRD_PLACE = 3;
+
+        //TODO ItThrowsAnArgumentExceptionIfAGivenPlayerHasMultipleRanks
+        //TODO ItThrowsAnArgumentExceptionIfThereAreMoreThanFifteenPlayers
+
         [Test]
         public void ItGivesTwoPointsToEachPlayerIfEveryoneLost()
         {
@@ -42,6 +49,224 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PointsTests
             Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
 
             Assert.That(actualPointsAwarded.All(x => x.Value == 2));
+        }
+
+        [Datapoint]
+        private readonly int[] numberOfPlayersArray = new[]
+        {
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            20
+        };
+
+        [Test]
+        [TestCaseSource("numberOfPlayersArray")]
+        public void ItGivesAboutTenPointsPerPlayerWhenRanksAreEvenlyDistributed(int numberOfPlayers)
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                playerRanks.Add(new PlayerRank
+                {
+                    GameRank = i + 1,
+                    PlayerId = i
+                });
+            }
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            //each player could round up at most 1 integer value
+            int minimumPointsAwarded = PointsCalculator.POINTS_PER_PLAYER * numberOfPlayers;
+            int maxPointsAwarded = PointsCalculator.POINTS_PER_PLAYER * numberOfPlayers + numberOfPlayers;
+            Assert.That(actualPointsAwarded.Sum(x => x.Value), Is.InRange(minimumPointsAwarded, maxPointsAwarded));
+        }
+
+        [Test]
+        [TestCaseSource("numberOfPlayersArray")]
+        public void ItGivesAboutTenPointsPerPlayerWhenRanksClumped(int numberOfPlayers)
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                playerRanks.Add(new PlayerRank
+                {
+                    GameRank = (i + 1) % 2,
+                    PlayerId = i
+                });
+            }
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            //each player could round up at most 1 integer value
+            int maxPointsAwarded = PointsCalculator.POINTS_PER_PLAYER * numberOfPlayers + numberOfPlayers;
+            Assert.That(actualPointsAwarded.Sum(x => x.Value), Is.InRange(PointsCalculator.POINTS_PER_PLAYER, maxPointsAwarded));
+        }
+
+        [Test]
+        public void ItGivesOutTenPointsToEachPlayerWhenEveryoneWins()
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+
+            for (int i = 0; i < 13; i++)
+            {
+                playerRanks.Add(new PlayerRank
+                {
+                    GameRank = FIRST_PLACE,
+                    PlayerId = i
+                });
+            }
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            Assert.That(actualPointsAwarded.All(x => x.Value == 10));
+        }
+
+        [Test]
+        public void ItWorksForOneWinnerAndOneLoser()
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            int winningPlayerId = 123;
+            int losingPlayerId = 456;
+
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = FIRST_PLACE,
+                PlayerId = winningPlayerId
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = SECOND_PLACE,
+                PlayerId = losingPlayerId
+            });
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            int expectedWinnerPoints = 14;//(int)Math.Ceiling(20m * (2m / 3m));
+            int expectedLoserPoints = 7;//(int)Math.Ceiling(20m * (1m / 3m));
+            Assert.That(actualPointsAwarded[winningPlayerId], Is.EqualTo(expectedWinnerPoints));
+            Assert.That(actualPointsAwarded[losingPlayerId], Is.EqualTo(expectedLoserPoints));
+        }
+
+        [Test]
+        public void ItWorksForTwoWinnersAndOneLoser()
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            int winningPlayerId1 = 123;
+            int winningPlayerId2 = 124;
+            int losingPlayerId = 456;
+
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = FIRST_PLACE,
+                PlayerId = winningPlayerId1
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = FIRST_PLACE,
+                PlayerId = winningPlayerId2
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = SECOND_PLACE,
+                PlayerId = losingPlayerId
+            });
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            int expectedWinnerPoints = 13;
+            int expectedLoserPoints = 5;
+            Assert.That(actualPointsAwarded[winningPlayerId1], Is.EqualTo(expectedWinnerPoints));
+            Assert.That(actualPointsAwarded[winningPlayerId2], Is.EqualTo(expectedWinnerPoints));
+            Assert.That(actualPointsAwarded[losingPlayerId], Is.EqualTo(expectedLoserPoints));
+        }
+
+        [Test]
+        public void ItWorksForOneWinnerAndTwoLosers()
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            int winningPlayerId1 = 123;
+            int losingPlayerId1 = 124;
+            int losingPlayerId2 = 456;
+
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = FIRST_PLACE,
+                PlayerId = winningPlayerId1
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = SECOND_PLACE,
+                PlayerId = losingPlayerId1
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = SECOND_PLACE,
+                PlayerId = losingPlayerId2
+            });
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            int expectedWinnerPoints = 15;
+            int expectedLoserPoints = 8;
+            Assert.That(actualPointsAwarded[winningPlayerId1], Is.EqualTo(expectedWinnerPoints));
+            Assert.That(actualPointsAwarded[losingPlayerId1], Is.EqualTo(expectedLoserPoints));
+            Assert.That(actualPointsAwarded[losingPlayerId2], Is.EqualTo(expectedLoserPoints));
+        }
+
+        [Test]
+        public void ItWorksForTwoWinnersTwoSecondPlayerAndOneLastPlace()
+        {
+            List<PlayerRank> playerRanks = new List<PlayerRank>();
+            int winningPlayerId1 = 100;
+            int winningPlayerId2 = 101;
+            int secondPlacePlayer1 = 200;
+            int secondPlacePlayer2 = 201;
+            int thirdPlacePlayerId = 300;
+
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = FIRST_PLACE,
+                PlayerId = winningPlayerId1
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = FIRST_PLACE,
+                PlayerId = winningPlayerId2
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = SECOND_PLACE,
+                PlayerId = secondPlacePlayer1
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = SECOND_PLACE,
+                PlayerId = secondPlacePlayer2
+            });
+            playerRanks.Add(new PlayerRank
+            {
+                GameRank = THIRD_PLACE,
+                PlayerId = thirdPlacePlayerId
+            });
+
+            Dictionary<int, int> actualPointsAwarded = PointsCalculator.CalculatePoints(playerRanks);
+
+            int expectedFirstPlacePoints = 18;
+            int expectedSecondPlacePoints = 7;
+            int expectedThirdPlacePoints = 3;
+            Assert.That(actualPointsAwarded[winningPlayerId1], Is.EqualTo(expectedFirstPlacePoints));
+            Assert.That(actualPointsAwarded[winningPlayerId2], Is.EqualTo(expectedFirstPlacePoints));
+            Assert.That(actualPointsAwarded[secondPlacePlayer1], Is.EqualTo(expectedSecondPlacePoints));
+            Assert.That(actualPointsAwarded[secondPlacePlayer2], Is.EqualTo(expectedSecondPlacePoints));
+            Assert.That(actualPointsAwarded[thirdPlacePlayerId], Is.EqualTo(expectedThirdPlacePoints));
+
         }
 
         //10 * (numberOfPlayers) * (fibonacciOf(numberOfPlayers + 1 - r) / fibonacciSumFrom(2, numberOfPlayers))
