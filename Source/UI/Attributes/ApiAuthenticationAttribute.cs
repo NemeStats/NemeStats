f44cb852f42dfc89f58383a7367c771a932b6080
Tosho Toshev <tosho_az@yahoo.com>
2015-03-25 21:43:33 -0400

    The new ApiAuthenticationAttribute is added to the solution

---
+++ Source/UI/Attributes/ApiAuthenticationAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLogic.Logic.Users;
+using StructureMap;
+using ActionFilterAttribute = System.Web.Http.Filters.ActionFilterAttribute;
+
+namespace UI.Attributes
+{
+    public class ApiAuthenticationAttribute : ActionFilterAttribute
+    {
+        private readonly ApplicationUserManager _applicationUserManager;
+        internal const string AUTH_HEADER = "X-Auth-Token";
+
+        public ApiAuthenticationAttribute()
+            : this(DependencyResolver.Current.GetService<ApplicationUserManager>())
+        {
+
+        }
+
+        public ApiAuthenticationAttribute(ApplicationUserManager applicationUserManager)
+        {
+            _applicationUserManager = applicationUserManager;
+        }
+
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (!actionContext.Request.Headers.Contains(AUTH_HEADER))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                return;
+            }
+
+            string authHeader = actionContext.Request.Headers.GetValues(AUTH_HEADER).FirstOrDefault();
+
+            if(string.IsNullOrWhiteSpace(authHeader))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                return;
+            }
+
+            if (!_applicationUserManager.Users.Any(x => x.AuthenticationToken == authHeader))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
---
+++ Tests/UI.Tests/UnitTests/AttributesTests/ApiAuthenticationAttributeTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using BusinessLogic.Logic.Users;
+using BusinessLogic.Models.User;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.DataProtection;
+using NUnit.Framework;
+using Rhino.Mocks;
+using StructureMap.AutoMocking;
+using UI.Attributes;
+
+namespace UI.Tests.UnitTests.AttributesTests
+{
+    [TestFixture]
+    public class ApiAuthenticationAttributeTests
+    {
+        private ApiAuthenticationAttribute _attribute;
+        private HttpActionContext _actionContext;
+        private HttpControllerContext _controllerContext;
+        private HttpRequestMessage _request;
+        private ApplicationUserManager _applicationUserManager;
+        private IUserStore<ApplicationUser> _userStoreMock;
+        private IDataProtectionProvider _dataProtectionProviderMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var userStoreMock = MockRepository.GenerateMock<IUserStore<ApplicationUser>>();
+            var dataProtector = MockRepository.GenerateMock<IDataProtector>();
+            _request = new HttpRequestMessage();
+            _userStoreMock = MockRepository.GenerateMock<IUserStore<ApplicationUser>>();
+            _dataProtectionProviderMock = MockRepository.GenerateMock<IDataProtectionProvider>();
+            _dataProtectionProviderMock.Expect(mock => mock.Create(Arg<string>.Is.Anything)).Return(dataProtector);
+            _applicationUserManager = MockRepository.GenerateMock<ApplicationUserManager>(_userStoreMock, _dataProtectionProviderMock);
+            _attribute = new ApiAuthenticationAttribute(_applicationUserManager);
+            _controllerContext = new HttpControllerContext {Request = _request};
+            _actionContext = new HttpActionContext(_controllerContext, new ReflectedHttpActionDescriptor());
+        }
+
+        [Test]
+        public void ShouldReturnBadRequestWhenNoTokenProvided()
+        {
+            _attribute.OnActionExecuting(_actionContext);
+            Assert.AreEqual(HttpStatusCode.BadRequest, _actionContext.Response.StatusCode);
+        }
+
+        [Test]
+        public void ShouldReturnBadRequestWhenTokenHasEmptyValue()
+        {
+            _request.Headers.Add(ApiAuthenticationAttribute.AUTH_HEADER, new [] { string.Empty });
+            _attribute.OnActionExecuting(_actionContext);
+            Assert.AreEqual(HttpStatusCode.BadRequest, _actionContext.Response.StatusCode);
+        }
+
+        [Test]
+        public void ShouldGetTheUsersToken()
+        {
+            const string expectedToken = "TEST";
+            _request.Headers.Add(ApiAuthenticationAttribute.AUTH_HEADER, new[] { expectedToken });
+            var expectedUsers = new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                    AuthenticationToken = expectedToken
+                }
+            };
+
+            _applicationUserManager.Expect(x => x.Users)
+                .Repeat.Any()
+                .Return(expectedUsers.AsQueryable());
+
+            _attribute.OnActionExecuting(_actionContext);
+            _applicationUserManager.AssertWasCalled(x => x.Users);
+        }
+
+        [Test]
+        public void ShouldReturnUnauthorizedHttpStatusWhenWronToken()
+        {
+            const string expectedToken = "TEST";
+            const string actualToken = "DIFFERENT";
+            _request.Headers.Add(ApiAuthenticationAttribute.AUTH_HEADER, new[] { expectedToken });
+            var expectedUsers = new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                    AuthenticationToken = actualToken
+                }
+            };
+
+            _applicationUserManager.Expect(x => x.Users)
+                .Repeat.Any()
+                .Return(expectedUsers.AsQueryable());
+
+            _attribute.OnActionExecuting(_actionContext);
+            Assert.AreEqual(HttpStatusCode.Unauthorized, _actionContext.Response.StatusCode);
+        }
+    }
+}
