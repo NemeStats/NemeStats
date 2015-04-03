c691e21aa126ba13ae52ce630e2656defa6cb2e3
Jake Gordon <jacob.gordon@Emerson.com>
2015-04-02 22:11:14 -0400

    finished POST UserSessions service

---
+++ Source/UI/Attributes/ApiModelValidationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http.ModelBinding;
+
+namespace UI.Attributes
+{
+    public class ApiModelValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ModelState.IsValid == false || actionContext.ActionArguments.All(x => x.Value == null))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(
+                    HttpStatusCode.BadRequest, "The request is invalid.");
+            }
+        }
+    }
+}
---
+++ Source/UI/Models/API/NewAuthTokenMessage.cs
@@ -0,0 +1,8 @@
+﻿
+namespace UI.Models.API
+{
+    public class NewAuthTokenMessage
+    {
+        public string AuthenticationToken { get; set; }
+    }
+}
