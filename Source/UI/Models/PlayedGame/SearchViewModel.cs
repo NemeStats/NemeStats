1109ffcb222ffeed9280bb955f2bc86c09fc16b6
Jake Gordon <jacob.gordon@Emerson.com>
2015-05-24 21:24:43 -0400

    partial commit

---
+++ Source/UI/Models/PlayedGame/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Models.PlayedGame
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<SelectListItem> GameDefinitions { get; set; }
+        public string DatePlayedStart { get; set; }
+        public string DatePlayedEnd { get; set; }
+    }
+}
