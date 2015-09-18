b8d0f8cf7d5a462a173464dfa0ca4c0164a9a5c8
Jake Gordon <jacob.gordon@Emerson.com>
2015-09-18 12:51:12 -0400

    GET /GameDefinitions is now version 2

--- Source/UI/Areas/Api/Controllers/GameDefinitionsController.cs
+++ Source/UI/Areas/Api/Controllers/GameDefinitionsController.cs
@@ -1,4 +1,24 @@
-﻿using BusinessLogic.Logic.GameDefinitions;
+﻿#region LICENSE
+// NemeStats is a free website for tracking the results of board games.
+//     Copyright (C) 2015 Jacob Gordon
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>
+#endregion
+
+using System;
+using System.Linq.Expressions;
+using BusinessLogic.Logic.GameDefinitions;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -22,7 +42,16 @@ namespace UI.Areas.Api.Controllers
 
         [ApiAuthentication]
         [ApiModelValidation]
-        [ApiRoute("GamingGroups/{gamingGroupId}/GameDefinitions/")]
+        [ApiRoute("GameDefinitions/", StartingVersion =  2)]
+        [HttpGet]
+        public virtual HttpResponseMessage GetGameDefinitionsVersion2([FromUri] int gamingGroupId)
+        {
+            return this.GetGameDefinitions(gamingGroupId);
+        }
+
+        [ApiAuthentication]
+        [ApiModelValidation]
+        [ApiRoute("GamingGroups/{gamingGroupId}/GameDefinitions/", AcceptedVersions = new[] { 1 })]
         [HttpGet]
         public virtual HttpResponseMessage GetGameDefinitions([FromUri] int gamingGroupId)
         {
