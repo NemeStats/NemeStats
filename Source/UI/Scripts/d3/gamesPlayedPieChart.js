f548b9e0eed12e9aa4457ff34689e97c7b881667
Michael Gerdes <mgerdes11@gmail.com>
2015-09-20 16:51:36 -0400

    Added a new pie chart that uses nvd3

--- Source/UI/Scripts/d3/gamesPlayedPieChart.js
+++ Source/UI/Scripts/d3/gamesPlayedPieChart.js
@@ -1,44 +1,16 @@
 ﻿function drawPieChart(summariesOfGames) {
-    var diameter = 360;
-    var radius = diameter / 2;
+    //Regular pie chart example
+    nv.addGraph(function () {
+        var chart = nv.models.pieChart()
+            .x(function (d) { return d.gameDefinitionName })
+            .y(function (d) { return d.gamesLost + d.gamesWon })
+            .showLabels(true);
 
-    var color = d3.scale.category20();
+        d3.select("#GamesPieChart svg")
+            .datum(summariesOfGames)
+            .transition().duration(350)
+            .call(chart);
 
-    var arc = d3.svg.arc()
-        .outerRadius(radius)
-        .innerRadius(0);
-
-    var pie = d3.layout.pie()
-        .value(function (summaryOfGame) {
-            return summaryOfGame.gamesLost + summaryOfGame.gamesWon;
-        })
-        .sort(null);
-
-    var svg = d3.select('#GamesPieChart')
-        .append('svg')
-            .attr('width', diameter)
-            .attr('height', diameter)
-        .append('g')
-            .attr('transform', 'translate(' + radius + ',' + radius + ')');
-
-    var g = svg.selectAll('.arc')
-        .data(pie(summariesOfGames))
-        .enter().append('g')
-            .attr('class', 'arc');
-
-    g.append('path')
-        .attr('d', arc)
-        .style('fill', function (gamePieSlice) {
-            return color(gamePieSlice.data.gameDefinitionName);
-        });
-
-    g.append('text')
-        .attr('transform', function (gamePieSlice) {
-            return 'translate(' + arc.centroid(gamePieSlice) + ')';
-        })
-        .attr('dy', '.35em')
-        .style('text-anchor', 'middle')
-        .text(function (gamePieSlice) {
-            return gamePieSlice.data.gameDefinitionName;
-        });
+        return chart;
+    });
 }
