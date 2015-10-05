aa3d684b541da8494748ab0d1a5c70111f1552da
Jake Gordon <jacob.gordon@Emerson.com>
2015-10-04 22:01:43 -0400

    fixed champion badge tooltip

---
+++ Source/UI/Models/Badges/ChampionBadgeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace UI.Models.Badges
+{
+    public class ChampionBadgeViewModel : IBadgeBaseViewModel
+    {
+        internal const string FONT_AWESOME_CSS_CLASS = "fa-trophy";
+        internal const string POPOVER_TEXT = "The Champion is the Player within the Gaming Group with the most wins of this game.";
+
+        public string GetFontAwesomeCssClass()
+        {
+            return FONT_AWESOME_CSS_CLASS;
+        }
+
+        public string GetPopoverText()
+        {
+            return POPOVER_TEXT;
+        }
+    }
+}
