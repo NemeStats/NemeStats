ec140723a19c3a5a50ac2770687cc995d6fb7e77
Michael Gerdes <mgerdes11@gmail.com>
2015-10-18 21:07:30 -0400

    Make the pie chart have a maximum of 10 slices.

--- Source/UI/Scripts/Player/playerDetails.js
+++ Source/UI/Scripts/Player/playerDetails.js
@@ -16,14 +16,38 @@ Views.Player.Details.prototype = {
             type: "GET",
             url: this.$apiUri,
             success: function (data) {
+                var gamesInPieChart = 9;
+
+                var pieChartData = data.gameDefinitionTotals.summariesOfGameDefinitionTotals;
+
+                pieChartData.sort(function (summary1, summary2) {
+                    return (summary2.gamesLost + summary2.gamesWon) - (summary1.gamesLost + summary1.gamesWon);
+                });
+
+                console.log(pieChartData);
+
+                if (pieChartData.length > gamesInPieChart) {
+                    var restOfGamesAccumulation = pieChartData.slice(gamesInPieChart).reduce(
+                        function (accumulativeSummary, summary) {
+                            return {
+                                gameDefinitionName: accumulativeSummary.gameDefinitionName,
+                                gamesLost: accumulativeSummary.gamesLost + summary.gamesLost,
+                                gamesWon: accumulativeSummary.gamesWon + summary.gamesWon
+                            };
+                        }, { gameDefinitionName: "Rest Of Games", gamesLost: 0, gamesWon: 0 });
+                    pieChartData = pieChartData.slice(0, gamesInPieChart).concat(restOfGamesAccumulation);
+                }
+
                 nv.addGraph(function () {
                     var chart = nv.models.pieChart()
                         .x(function (d) { return d.gameDefinitionName; })
                         .y(function (d) { return d.gamesLost + d.gamesWon; })
-                        .showLabels(false);
+                        .showLabels(false)
+                          .legendPosition("top");
+                    ;
 
                     d3.select("#GamesPieChart svg")
-                        .datum(data.gameDefinitionTotals.summariesOfGameDefinitionTotals)
+                        .datum(pieChartData)
                         .transition().duration(350)
                         .call(chart);
 
