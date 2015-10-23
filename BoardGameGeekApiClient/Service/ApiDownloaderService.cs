7f6efb00c376eeb5a7c1b9e5925e826313da1fee
vfernandez <vfernandez@avanzis.com>
2015-10-23 14:42:49 +0200

    Removed retry policy and reduced to 10 seconds the timeout on ApiDownloaderService

--- BoardGameGeekApiClient/Service/ApiDownloaderService.cs
+++ BoardGameGeekApiClient/Service/ApiDownloaderService.cs
@@ -17,35 +17,22 @@ namespace BoardGameGeekApiClient.Service
             // UTF-8 is now hard-coded...
 
             XDocument data = null;
-            var retries = 0;
-            while (data == null && retries < 60)
+            while (data == null )
             {
-                retries++;
                 var request = WebRequest.CreateHttp(requestUrl);
-                request.Timeout = 15000;
+                request.Timeout = 10000;
                 using (var response = (HttpWebResponse)(request.GetResponse()))
                 {
-                    if (response.StatusCode == HttpStatusCode.OK)
-                    {
+                    if (response.StatusCode != HttpStatusCode.OK) continue;
 
-                        using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                        {
-                            data = XDocument.Parse(reader.ReadToEnd());
-                        }
+                    using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        data = XDocument.Parse(reader.ReadToEnd());
                     }
                 }
             }
 
-            if (data != null)
-            {
-                return data;
-            }
-            else
-            {
-                throw new Exception("Failed to download BGG data.");
-            }
-
-
+            return data;
         }
     }
 }
