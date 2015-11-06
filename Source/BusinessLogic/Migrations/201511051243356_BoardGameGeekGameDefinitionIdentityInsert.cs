ed205f42d29c0297d7c1787db671958dde46b81c
Jake Gordon <jacob.gordon@Emerson.com>
2015-11-05 23:27:02 -0500

    fixed migrations

---
+++ Source/BusinessLogic/Migrations/201511051243356_BoardGameGeekGameDefinitionIdentityInsert.cs
@@ -0,0 +1,26 @@
+namespace BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BoardGameGeekGameDefinitionIdentityInsert : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.GameDefinition", "BoardGameGeekGameDefinitionId", "dbo.BoardGameGeekGameDefinition");
+            //DropPrimaryKey("dbo.BoardGameGeekGameDefinition");
+            AlterColumn("dbo.BoardGameGeekGameDefinition", "Id", c => c.Int(nullable: false));
+            //AddPrimaryKey("dbo.BoardGameGeekGameDefinition", "Id");
+            AddForeignKey("dbo.GameDefinition", "BoardGameGeekGameDefinitionId", "dbo.BoardGameGeekGameDefinition", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.GameDefinition", "BoardGameGeekGameDefinitionId", "dbo.BoardGameGeekGameDefinition");
+            //DropPrimaryKey("dbo.BoardGameGeekGameDefinition");
+            AlterColumn("dbo.BoardGameGeekGameDefinition", "Id", c => c.Int(nullable: false, identity: true));
+            //AddPrimaryKey("dbo.BoardGameGeekGameDefinition", "Id");
+            AddForeignKey("dbo.GameDefinition", "BoardGameGeekGameDefinitionId", "dbo.BoardGameGeekGameDefinition", "Id");
+        }
+    }
+}
