dc385e3ad33eb23d89ec4203fe19301d690db3b7
Víctor Fernández Portero <vfportero@gmail.com>
2016-01-13 10:40:31 +0100

    Modernizr fallback for input dates non compatible browser

--- Source/UI/Scripts/CreatePlayedGame/createplayedgame.js
+++ Source/UI/Scripts/CreatePlayedGame/createplayedgame.js
@@ -3,273 +3,287 @@ Namespace("Views.PlayedGame");
 
 //Initialization
 Views.PlayedGame.CreatePlayedGame = function () {
-	this._playerRank = 1;
-	this._playerIndex = 0;
-	this.$recordPlayedGameForm = null;
-	this.$rankedPlayers = null;
-	this.$rankedPlayersListItem = null;
-	this.$playerId = null;
-	this.$players = null;
-	this.$playersErrorContainer = null;
-	this.$playersErrorRankTooHigh = null;
-	this.$playerFormData = null;
-	this.$btnAddPlayer = null;
-	this.$addPlayer = null;
-	this.$playerItemTemplate = null;
-	this._googleAnalytics = null;
-	this._rankButtons = null;
-	this.$gameDefinitionDropDown = null;
-	this.$gameId = null;
-	this.$btnEveryoneWonButton = null;
-	this.$btnEveryoneLostButton = null;
-	this.$removePlayerButtons = null;
-	this.$buttonUp = null;
-	this.$buttonDown = null;
-	this.$document = null;
-	this.$numRankedPlayers = null;
+    this._playerRank = 1;
+    this._playerIndex = 0;
+    this.$recordPlayedGameForm = null;
+    this.$rankedPlayers = null;
+    this.$rankedPlayersListItem = null;
+    this.$playerId = null;
+    this.$players = null;
+    this.$playersErrorContainer = null;
+    this.$playersErrorRankTooHigh = null;
+    this.$playerFormData = null;
+    this.$btnAddPlayer = null;
+    this.$addPlayer = null;
+    this.$datePicker = null;
+    this.$playerItemTemplate = null;
+    this._googleAnalytics = null;
+    this._rankButtons = null;
+    this.$gameDefinitionDropDown = null;
+    this.$gameId = null;
+    this.$btnEveryoneWonButton = null;
+    this.$btnEveryoneLostButton = null;
+    this.$removePlayerButtons = null;
+    this.$buttonUp = null;
+    this.$buttonDown = null;
+    this.$document = null;
+    this.$numRankedPlayers = null;
 };
 
 //Implementation
 Views.PlayedGame.CreatePlayedGame.prototype = {
-	//Method definitions
-	init: function (gaObject, rankedPlayers) {
-		//Fields
-		var parent = this;
-		this._rankButtons = [];
-		this.$recordPlayedGameForm = $("#recordPlayedGame");
-		this.$playersErrorContainer = $("#players-error");
-		this.$playersErrorRankTooHigh = $("#players-error-rank-too-high");
-		this.$rankedPlayers = $("#rankedPlayers");
-		this.$players = $("#Players");
-		this.$playerFormData = $("#playerFormData");
-		this.$playerDiv = $("#playerDiv");
-		this.$addPlayer = $("#addPlayer");
-		this.$btnAddPlayer = $("#btnAddPlayer");
-		this.$anchorAddPlayer = $("#addPlayerAnchor");
-		this.$gameDefinitionDropDown = $("[name='GameDefinitionId']");
-		this.$btnEveryoneWonButton = $("#btnEveryoneWonButton");
-		this.$btnEveryoneLostButton = $("#btnEveryoneLostButton");
-		this.$removePlayerButtons = $(".btnRemovePlayer");
-		this.$buttonUp = $(".rankButton-up");
-		this.$buttonDown = $(".rankButton-down");
-		this.$document = $(document);
-		this.$numRankedPlayers = 0;
+    //Method definitions
+    init: function (gaObject, rankedPlayers) {
+        //Fields
+        var parent = this;
+        this._rankButtons = [];
+        this.$recordPlayedGameForm = $("#recordPlayedGame");
+        this.$playersErrorContainer = $("#players-error");
+        this.$playersErrorRankTooHigh = $("#players-error-rank-too-high");
+        this.$rankedPlayers = $("#rankedPlayers");
+        this.$players = $("#Players");
+        this.$playerFormData = $("#playerFormData");
+        this.$playerDiv = $("#playerDiv");
+        this.$addPlayer = $("#addPlayer");
+        this.$btnAddPlayer = $("#btnAddPlayer");
+        this.$anchorAddPlayer = $("#addPlayerAnchor");
+        this.$gameDefinitionDropDown = $("[name='GameDefinitionId']");
 
-		this._googleAnalytics = gaObject;
+        if (!Modernizr.inputtypes.date) {
+            // If not native HTML5 support, fallback to jQuery datePicker
+            this.$datePicker = $(".date-picker").datepicker({
+                showOn: "button",
+                buttonText: "<i class='fa fa-calendar'></i>",
+                showButtonPanel: true,
+                maxDate: new Date(),
+                minDate: new Date(2014, 1, 1)
+            }).datepicker("setDate", new Date())
+    .datepicker("option", "dateFormat", "yy-mm-dd");
+        }
 
-		this.$recordPlayedGameForm.on("submit", $.proxy(parent.validatePlayers, parent));
+        this.$btnEveryoneWonButton = $("#btnEveryoneWonButton");
+        this.$btnEveryoneLostButton = $("#btnEveryoneLostButton");
+        this.$removePlayerButtons = $(".btnRemovePlayer");
+        this.$buttonUp = $(".rankButton-up");
+        this.$buttonDown = $(".rankButton-down");
+        this.$document = $(document);
+        this.$numRankedPlayers = 0;
 
-		var currentRank = this.$rankedPlayers.attr("data-numRankedPlayers");
+        this._googleAnalytics = gaObject;
 
-		if (currentRank > 0)
-			this._playerRank = currentRank;
+        this.$recordPlayedGameForm.on("submit", $.proxy(parent.validatePlayers, parent));
 
-		//Event handlers
-		this.$players.change(function () { parent.addPlayer(); });
-		this.$rankedPlayers.sortable({
-			stop: function () {
-				parent.onReorder();
-			}
-		});
+        var currentRank = this.$rankedPlayers.attr("data-numRankedPlayers");
 
-		this.$btnAddPlayer.on("click", function () {
-			if (parent.$addPlayer.hasClass("hidden")) {
-				parent.$addPlayer.removeClass("hidden");
-			} else {
-				parent.$addPlayer.addClass("hidden");
-			}
-			document.location = parent.$anchorAddPlayer.attr("href");
-			parent._googleAnalytics.trackGAEvent("PlayedGames", "AddNewPlayerClicked", "AddNewPlayerClicked");
-		});
+        if (currentRank > 0)
+            this._playerRank = currentRank;
 
-		this.$btnEveryoneWonButton.on("click", function () {
-			if (confirm("Are you sure you want to give every player a win?")) {
-				$(".playerRank,input[name$='.GameRank'").val(1);
-				parent._googleAnalytics.trackGAEvent("PlayedGames", "TeamWinRecorded", "TeamWinRecorded");
-				return true;
-			} else {
-				return false;
-			}
-		});
+        //Event handlers
+        this.$players.change(function () { parent.addPlayer(); });
+        this.$rankedPlayers.sortable({
+            stop: function () {
+                parent.onReorder();
+            }
+        });
 
-		this.$btnEveryoneLostButton.on("click", function () {
-			if (confirm("Are you sure you want to give every player a loss?")) {
-				$(".playerRank,input[name$='.GameRank'").val(2);
-				parent._googleAnalytics.trackGAEvent("PlayedGames", "TeamLossRecorded", "TeamLossRecorded");
-				return true;
-			} else {
-				return false;
-			}
-		});
+        this.$btnAddPlayer.on("click", function () {
+            if (parent.$addPlayer.hasClass("hidden")) {
+                parent.$addPlayer.removeClass("hidden");
+            } else {
+                parent.$addPlayer.addClass("hidden");
+            }
+            document.location = parent.$anchorAddPlayer.attr("href");
+            parent._googleAnalytics.trackGAEvent("PlayedGames", "AddNewPlayerClicked", "AddNewPlayerClicked");
+        });
 
-		this.$playerItemTemplate = $("#player-item-template");
+        this.$btnEveryoneWonButton.on("click", function () {
+            if (confirm("Are you sure you want to give every player a win?")) {
+                $(".playerRank,input[name$='.GameRank'").val(1);
+                parent._googleAnalytics.trackGAEvent("PlayedGames", "TeamWinRecorded", "TeamWinRecorded");
+                return true;
+            } else {
+                return false;
+            }
+        });
 
-		var shared = new Views.Shared.Layout();
-		this.$gameId = shared.getQueryString("gameId");
+        this.$btnEveryoneLostButton.on("click", function () {
+            if (confirm("Are you sure you want to give every player a loss?")) {
+                $(".playerRank,input[name$='.GameRank'").val(2);
+                parent._googleAnalytics.trackGAEvent("PlayedGames", "TeamLossRecorded", "TeamLossRecorded");
+                return true;
+            } else {
+                return false;
+            }
+        });
 
-		parent.validateGameDefinition();
+        this.$playerItemTemplate = $("#player-item-template");
 
-		this.setupButtons();
-		this.initializeRankedPlayers(rankedPlayers);
-	},
-	onReorder: function () {
-		var parent = this;
-		this.$rankedPlayersListItem = $("#rankedPlayers li");
+        var shared = new Views.Shared.Layout();
+        this.$gameId = shared.getQueryString("gameId");
 
-		var removePlayerButtons = $(".btnRemovePlayer");
-		removePlayerButtons.off('click').on("click", function () {
-			parent.removePlayer(this);
-		});
+        parent.validateGameDefinition();
 
-		this.recalculateRanks();
-		this._googleAnalytics.trackGAEvent("PlayedGames", "PlayersReordered", "PlayersReordered");
-	},
-	generatePlayerRankListItemString: function (playerIndex, playerId, playerName, playerRank) {
-		var template = Handlebars.compile(this.$playerItemTemplate.html());
-		var context = { playerIndex: playerIndex, playerId: playerId, playerName: playerName, playerRank: playerRank };
+        this.setupButtons();
+        this.initializeRankedPlayers(rankedPlayers);
+    },
+    onReorder: function () {
+        var parent = this;
+        this.$rankedPlayersListItem = $("#rankedPlayers li");
 
-		return template(context);
-	},
-	initializeRankedPlayers: function (rankedPlayers) {
-		if (rankedPlayers != null) {
-			for (var i = 0; i < rankedPlayers.length; i++) {
-				var playerItem = this.generatePlayerRankListItemString(rankedPlayers[i].playerIndex, rankedPlayers[i].playerId, rankedPlayers[i].playerName, rankedPlayers[i].playerRank);
-				this.$rankedPlayers.append(playerItem);
-				this.$numRankedPlayers++;
-			}
-		}
-	},
-	addPlayer: function () {
-		var selectedOption = this.$players.find(":selected");
-		this.$playersErrorContainer.addClass("hidden");
-		this.$playersErrorRankTooHigh.addClass("hidden");
+        var removePlayerButtons = $(".btnRemovePlayer");
+        removePlayerButtons.off('click').on("click", function () {
+            parent.removePlayer(this);
+        });
 
-		if (selectedOption.text() == "Add A Player") {
-			return;
-		}
+        this.recalculateRanks();
+        this._googleAnalytics.trackGAEvent("PlayedGames", "PlayersReordered", "PlayersReordered");
+    },
+    generatePlayerRankListItemString: function (playerIndex, playerId, playerName, playerRank) {
+        var template = Handlebars.compile(this.$playerItemTemplate.html());
+        var context = { playerIndex: playerIndex, playerId: playerId, playerName: playerName, playerRank: playerRank };
 
-		var playerId = selectedOption.val();
-		var playerName = selectedOption.text();
-		var playerItem = this.generatePlayerRankListItemString(this._playerIndex, playerId, playerName, this._playerRank);
+        return template(context);
+    },
+    initializeRankedPlayers: function (rankedPlayers) {
+        if (rankedPlayers != null) {
+            for (var i = 0; i < rankedPlayers.length; i++) {
+                var playerItem = this.generatePlayerRankListItemString(rankedPlayers[i].playerIndex, rankedPlayers[i].playerId, rankedPlayers[i].playerName, rankedPlayers[i].playerRank);
+                this.$rankedPlayers.append(playerItem);
+                this.$numRankedPlayers++;
+            }
+        }
+    },
+    addPlayer: function () {
+        var selectedOption = this.$players.find(":selected");
+        this.$playersErrorContainer.addClass("hidden");
+        this.$playersErrorRankTooHigh.addClass("hidden");
 
-		this.$rankedPlayers.append(playerItem);
-		this._playerIndex++;
-		this._playerRank++;
-		this.$numRankedPlayers++;
-		selectedOption.remove();
+        if (selectedOption.text() == "Add A Player") {
+            return;
+        }
 
-		this.recalculateRanks();
+        var playerId = selectedOption.val();
+        var playerName = selectedOption.text();
+        var playerItem = this.generatePlayerRankListItemString(this._playerIndex, playerId, playerName, this._playerRank);
 
-		return null;
-	},
-	setupButtons: function () {
-		var parent = this;
+        this.$rankedPlayers.append(playerItem);
+        this._playerIndex++;
+        this._playerRank++;
+        this.$numRankedPlayers++;
+        selectedOption.remove();
 
-		this.$document.on("click", ".btnRemovePlayer", function () {
-			parent.removePlayer(this);
-		});
+        this.recalculateRanks();
 
-		this.$document.on("click", ".rankButton-up", function () {
-			parent.movePlayerUp(this);
-		});
+        return null;
+    },
+    setupButtons: function () {
+        var parent = this;
 
-		this.$document.on("click", ".rankButton-down", function () {
-			parent.movePlayerDown(this);
-		});
+        this.$document.on("click", ".btnRemovePlayer", function () {
+            parent.removePlayer(this);
+        });
 
-		this.recalculateRanks();
-	},
-	removePlayer: function (button) {
-		var playerId = $(button).data("playerid");
-		var playerName = $(button).data("playername");
-		$(button).parents('li').remove();
-		this._playerRank--;
-		this.$numRankedPlayers--;
-		var newPlayer = $('<option value="' + playerId + '">' + playerName + '</option>');
-		this.$players.append(newPlayer);
-		this.recalculateRanks();
+        this.$document.on("click", ".rankButton-up", function () {
+            parent.movePlayerUp(this);
+        });
 
-		this._googleAnalytics.trackGAEvent("PlayedGames", "PlayerRemoved", "PlayerRemoved");
-	},
-	movePlayerUp: function (button) {
-		var item = $(button).closest("li");
-		var previous = item.prev();
+        this.$document.on("click", ".rankButton-down", function () {
+            parent.movePlayerDown(this);
+        });
 
-		if (previous.length > 0) {
-			item.insertBefore(previous);
-			this.recalculateRanks();
-			this._googleAnalytics.trackGAEvent("PlayedGames", "PlayersReorderedViaArrow", "PlayersReorderedViaArrow");
-		}
-	},
-	movePlayerDown: function (button) {
-		var item = $(button).closest("li");
-		var next = item.next();
+        this.recalculateRanks();
+    },
+    removePlayer: function (button) {
+        var playerId = $(button).data("playerid");
+        var playerName = $(button).data("playername");
+        $(button).parents('li').remove();
+        this._playerRank--;
+        this.$numRankedPlayers--;
+        var newPlayer = $('<option value="' + playerId + '">' + playerName + '</option>');
+        this.$players.append(newPlayer);
+        this.recalculateRanks();
 
-		if (next.length > 0) {
-			item.insertAfter(next);
-			this.recalculateRanks();
-			this._googleAnalytics.trackGAEvent("PlayedGames", "PlayersReorderedViaArrow", "PlayersReorderedViaArrow");
-		}
-	},
-	recalculateRanks: function () {
-		var playerItems = this.$rankedPlayers.children();
-		var idx = 1;
-		for (var i = 0; i < playerItems.length; i++) {
-			var playerItem = $(playerItems[i]);
-			var inputTextField = playerItem.find("input:text");
-			var inputHiddenField = playerItem.find("input:hidden");
-			inputTextField.val(idx);
-			inputTextField.attr("name", "PlayerRanks[" + i + "].GameRank");
-			inputHiddenField.attr("name", "PlayerRanks[" + i + "].PlayerId");
-			idx++;
-		}
-	},
-	onPlayerCreated: function (player) {
-		var newPlayer = $('<option value="' + player.Id + '">' + player.Name + '</option>');
-		this.$players.append(newPlayer);
+        this._googleAnalytics.trackGAEvent("PlayedGames", "PlayerRemoved", "PlayerRemoved");
+    },
+    movePlayerUp: function (button) {
+        var item = $(button).closest("li");
+        var previous = item.prev();
 
-		this._googleAnalytics.trackGAEvent("PlayedGames", "NewPlayerAdded", "NewPlayerAdded");
-	},
+        if (previous.length > 0) {
+            item.insertBefore(previous);
+            this.recalculateRanks();
+            this._googleAnalytics.trackGAEvent("PlayedGames", "PlayersReorderedViaArrow", "PlayersReorderedViaArrow");
+        }
+    },
+    movePlayerDown: function (button) {
+        var item = $(button).closest("li");
+        var next = item.next();
 
-	validatePlayers: function (event) {
-		if (this.$numRankedPlayers < 2) {
-			this.$playersErrorContainer.removeClass("hidden");
-			return false;
-		}
-		this.$playersErrorContainer.addClass("hidden");
+        if (next.length > 0) {
+            item.insertAfter(next);
+            this.recalculateRanks();
+            this._googleAnalytics.trackGAEvent("PlayedGames", "PlayersReorderedViaArrow", "PlayersReorderedViaArrow");
+        }
+    },
+    recalculateRanks: function () {
+        var playerItems = this.$rankedPlayers.children();
+        var idx = 1;
+        for (var i = 0; i < playerItems.length; i++) {
+            var playerItem = $(playerItems[i]);
+            var inputTextField = playerItem.find("input:text");
+            var inputHiddenField = playerItem.find("input:hidden");
+            inputTextField.val(idx);
+            inputTextField.attr("name", "PlayerRanks[" + i + "].GameRank");
+            inputHiddenField.attr("name", "PlayerRanks[" + i + "].PlayerId");
+            idx++;
+        }
+    },
+    onPlayerCreated: function (player) {
+        var newPlayer = $('<option value="' + player.Id + '">' + player.Name + '</option>');
+        this.$players.append(newPlayer);
 
-		var allRankValues = this.$rankedPlayers.children().map(function () {
-			return $(this).find("input[type=text]").val();
-		});
-		if (Math.max.apply(Math, allRankValues) > this.$rankedPlayers.children().length) {
-			this.$playersErrorRankTooHigh.removeClass("hidden");
-			return false;
-		}
-		this.$playersErrorRankTooHigh.addClass("hidden");
+        this._googleAnalytics.trackGAEvent("PlayedGames", "NewPlayerAdded", "NewPlayerAdded");
+    },
 
-		return true;
-	},
+    validatePlayers: function (event) {
+        if (this.$numRankedPlayers < 2) {
+            this.$playersErrorContainer.removeClass("hidden");
+            return false;
+        }
+        this.$playersErrorContainer.addClass("hidden");
 
-	validateGameDefinition: function () {
-		if ($("#gameDefinitionDropDown").data("numofdefinitions") == 0) {
-			$("#gameDefinitionDropDown").popover({ html: true });
-			$("#gameDefinitionDropDown").popover("show");
-			$(":input").prop("disabled", true);
-		} else if (this.$gameId != "")
-			this.$gameDefinitionDropDown.val(this.$gameId);
-	},
+        var allRankValues = this.$rankedPlayers.children().map(function () {
+            return $(this).find("input[type=text]").val();
+        });
+        if (Math.max.apply(Math, allRankValues) > this.$rankedPlayers.children().length) {
+            this.$playersErrorRankTooHigh.removeClass("hidden");
+            return false;
+        }
+        this.$playersErrorRankTooHigh.addClass("hidden");
 
-	//Properties
-	set_playerIndex: function (value) {
-		this._playerIndex = value;
-	},
-	get_playerIndex: function () {
-		return this._playerIndex;
-	},
-	set_playerRank: function (value) {
-		this._playerRank = value;
-	},
-	get_playerRank: function () {
-		return this._playerRank;
-	}
+        return true;
+    },
+
+    validateGameDefinition: function () {
+        if ($("#gameDefinitionDropDown").data("numofdefinitions") == 0) {
+            $("#gameDefinitionDropDown").popover({ html: true });
+            $("#gameDefinitionDropDown").popover("show");
+            $(":input").prop("disabled", true);
+        } else if (this.$gameId != "")
+            this.$gameDefinitionDropDown.val(this.$gameId);
+    },
+
+    //Properties
+    set_playerIndex: function (value) {
+        this._playerIndex = value;
+    },
+    get_playerIndex: function () {
+        return this._playerIndex;
+    },
+    set_playerRank: function (value) {
+        this._playerRank = value;
+    },
+    get_playerRank: function () {
+        return this._playerRank;
+    }
 }; //end prototypes
---
+++ Source/UI/Scripts/PlayedGame/search.js
@@ -0,0 +1,25 @@
+﻿//Usings
+Namespace("Views.PlayedGame");
+
+//Initialization
+Views.PlayedGame.Search = function () {
+    this.$datePicker = null;
+};
+
+//Implementation
+Views.PlayedGame.Search.prototype = {
+    //Method definitions
+    init: function () {
+        //Fields
+        if (!Modernizr.inputtypes.date) {
+            // If not native HTML5 support, fallback to jQuery datePicker
+            this.$datePicker = $(".date-picker").datepicker({
+                showOn: "button",
+                buttonText: "<i class='fa fa-calendar'></i>",
+                showButtonPanel: true,
+                minDate: new Date(2014, 1, 1)
+            });
+        }
+        
+    }
+}; //end prototypes
