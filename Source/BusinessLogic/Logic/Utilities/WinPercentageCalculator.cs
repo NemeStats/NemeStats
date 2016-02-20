b66a03e3d1e8cbeed761f2b8ad3a85b40d98fcec
Jake Gordon <jacob.gordon@Emerson.com>
2016-02-19 22:01:53 -0500

    fixed top players widget win percentage

--- Source/BusinessLogic/Logic/Utilities/WinPercentageCalculator.cs
+++ Source/BusinessLogic/Logic/Utilities/WinPercentageCalculator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessLogic.Logic.Utilities
+﻿namespace BusinessLogic.Logic.Utilities
 {
     public static class WinPercentageCalculator
     {
