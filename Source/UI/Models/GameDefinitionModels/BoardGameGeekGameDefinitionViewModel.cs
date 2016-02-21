8e938cd071d23768a12400ce79b7e654fec3dc11
Víctor Fernández Portero <vfportero@gmail.com>
2016-02-21 21:05:55 +0100

    gamedefinition details view with BGG data

--- Source/UI/Models/GameDefinitionModels/BoardGameGeekGameDefinitionViewModel.cs
+++ Source/UI/Models/GameDefinitionModels/BoardGameGeekGameDefinitionViewModel.cs
@@ -4,6 +4,8 @@ namespace UI.Models.GameDefinitionModels
 {
     public class BoardGameGeekGameDefinitionViewModel
     {
+        private const int MaxBggWeight = 5;
+
         public int? BoardGameGeekGameDefinitionId { get; set; }
         public string Name { get; set; }
         public string Thumbnail { get; set; }
@@ -14,5 +16,50 @@ namespace UI.Models.GameDefinitionModels
         public string Description { get; set; }
 
         public Uri BoardGameGeekUri { get; set; }
+
+        public string WeightPercent
+        {
+            get
+            {
+                if (AverageWeight.HasValue)
+                {
+                    
+                    return ((AverageWeight.Value * 100) / MaxBggWeight).ToString().Replace(",",".");
+                }
+                return "0";
+            }
+        }
+
+        public string WeightDescription
+        {
+            get
+            {
+                if (AverageWeight.HasValue)
+                {
+                    var weight = (double) AverageWeight.Value;
+                    if (weight < 1.5)
+                    {
+                        return "Casual";
+                    }
+                    if (weight < 2)
+                    {
+                        return "Easy";
+                    }
+                    if (weight < 3)
+                    {
+                        return "Normal";
+                    }
+                    if (weight < 4)
+                    {
+                        return "Hardcore";
+                    }
+                    if (weight < 5)
+                    {
+                        return "Nightmare";
+                    }
+                }
+                return string.Empty;
+            } 
+        }
     }
 }
