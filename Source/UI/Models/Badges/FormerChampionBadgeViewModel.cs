f87ac727f3cb605516ca3fb81d77109cb08dc484
vfernandez <vfernandez@avanzis.com>
2016-02-24 12:02:54 +0100

    Fixed wrong former champion ico

--- Source/UI/Models/Badges/FormerChampionBadgeViewModel.cs
+++ Source/UI/Models/Badges/FormerChampionBadgeViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class FormerChampionBadgeViewModel : IBadgeBaseViewModel
     {
-        internal const string FONT_AWESOME_CSS_CLASS = "fa fa-trophy fa-slashed";
+        internal const string FONT_AWESOME_CSS_CLASS = "ns-icon-former-champion";
         internal const string POPOVER_TEXT = "This Player is the former Champion of this game.";
 
         public string GetIconCssClass()
