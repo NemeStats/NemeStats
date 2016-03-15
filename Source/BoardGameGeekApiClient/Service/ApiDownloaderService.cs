8542d77496ccc825b75df3cf42f9722a97cca896
Víctor Fernández Portero <vfportero@gmail.com>
2016-03-15 17:35:53 +0100

    BGGTransientErrorDetectionStrategy

--- Source/BoardGameGeekApiClient/Service/ApiDownloaderService.cs
+++ Source/BoardGameGeekApiClient/Service/ApiDownloaderService.cs
@@ -5,19 +5,49 @@ using System.Net;
 using System.Text;
 using System.Xml.Linq;
 using BoardGameGeekApiClient.Interfaces;
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
 
 namespace BoardGameGeekApiClient.Service
 {
+    internal class BGGTransientErrorDetectionStrategy : ITransientErrorDetectionStrategy
+    {
+        public bool IsTransient(Exception ex)
+        {
+            if (ex is WebException)
+                return true;
+            return false;
+        }
+    }
+
     public class ApiDownloaderService : IApiDownloadService
     {
+        private readonly RetryPolicy<BGGTransientErrorDetectionStrategy> _retryPolicy;
+
+        public ApiDownloaderService()
+        {
+            var retryStrategy = new Incremental(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5));
+            _retryPolicy = new RetryPolicy<BGGTransientErrorDetectionStrategy>(retryStrategy);
+        }
+
         public XDocument DownloadApiResult(Uri requestUrl)
         {
             Debug.WriteLine("Downloading " + requestUrl);
+
+            
+
+            XDocument result = null;
+            _retryPolicy.ExecuteAction(() => ReadApiResult(requestUrl, out result));
+
+
+            return result;
+        }
+
+        private static void ReadApiResult(Uri requestUrl, out XDocument data)
+        {
             // Due to malformed header I cannot use GetContentAsync and ReadAsStringAsync :(
             // UTF-8 is now hard-coded...
-
-            XDocument data = null;
-            while (data == null )
+            data = null;
+            while (data == null)
             {
                 var request = WebRequest.CreateHttp(requestUrl);
                 request.Timeout = 10000;
@@ -31,8 +61,6 @@ namespace BoardGameGeekApiClient.Service
                     }
                 }
             }
-
-            return data;
         }
     }
 }
