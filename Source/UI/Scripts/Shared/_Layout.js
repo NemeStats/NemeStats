f9314194758e9d170673f2ccb6f3a4def32fb863
Víctor Fernández Portero <vfportero@gmail.com>
2016-04-19 21:51:44 +0200

    NemePoints breakdown popover

--- Source/UI/Scripts/Shared/_Layout.js
+++ Source/UI/Scripts/Shared/_Layout.js
@@ -12,17 +12,27 @@ Views.Shared.Layout = function () {
 Views.Shared.Layout.prototype = {
     init: function () {
         this.$popovers = $('[data-toggle="popover"]');
-        this.$popovers.popover({ trigger: 'hover' });
+        $.each(this.$popovers, function (i, item) {
+            var $item = $(item);
+            if ($item.data("templateselector")) {
+                var content = $($item.data("templateselector")).html();
+                $item.popover({ trigger: 'click', content: content, placement: 'top', container: "body" });
+                $item.addClass("clickable");
+            } else {
+                $item.popover({ trigger: 'hover',container: "body" });
+            }
+        });
+
 
         this.$readmoreContainers = $("[data-readmore='true']");
-        this.$readmoreContainers.readmore({ collapsedHeight: 100, lessLink: '<a href="#">Read less</a>' ,speed: 200});
+        this.$readmoreContainers.readmore({ collapsedHeight: 100, lessLink: '<a href="#">Read less</a>', speed: 200 });
     },
     getQueryString: function (queryParamName) {
-    	var result = window.location.search.match(
+        var result = window.location.search.match(
 			new RegExp("(\\?|&)" + queryParamName + "(\\[\\])?=([^&]*)")
 		);
 
-    	return result ? result[3] : false;
+        return result ? result[3] : false;
     }
 };
 
