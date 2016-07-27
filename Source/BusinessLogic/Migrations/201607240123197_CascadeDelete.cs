8472f1772c33058a305942409bd4a5cdd39a6596
jakejgordon <jakejgordon@gmail.com>
2016-07-27 08:01:19 -0400

    Temporarily disabling failing migration and custom errors

--- Source/BusinessLogic/Migrations/201607240123197_CascadeDelete.cs
+++ Source/BusinessLogic/Migrations/201607240123197_CascadeDelete.cs
@@ -7,20 +7,20 @@ namespace BusinessLogic.Migrations
     {
         public override void Up()
         {
-            DropForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player");
-            DropIndex("dbo.GamingGroupInvitation", new[] { "PlayerId" });
-            AlterColumn("dbo.GamingGroupInvitation", "PlayerId", c => c.Int(nullable: false));
-            CreateIndex("dbo.GamingGroupInvitation", "PlayerId");
-            AddForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player", "Id", cascadeDelete: true);
+            //DropForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player");
+            //DropIndex("dbo.GamingGroupInvitation", new[] { "PlayerId" });
+            //AlterColumn("dbo.GamingGroupInvitation", "PlayerId", c => c.Int(nullable: false));
+            //CreateIndex("dbo.GamingGroupInvitation", "PlayerId");
+            //AddForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player", "Id", cascadeDelete: true);
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player");
-            DropIndex("dbo.GamingGroupInvitation", new[] { "PlayerId" });
-            AlterColumn("dbo.GamingGroupInvitation", "PlayerId", c => c.Int());
-            CreateIndex("dbo.GamingGroupInvitation", "PlayerId");
-            AddForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player", "Id");
+            //DropForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player");
+            //DropIndex("dbo.GamingGroupInvitation", new[] { "PlayerId" });
+            //AlterColumn("dbo.GamingGroupInvitation", "PlayerId", c => c.Int());
+            //CreateIndex("dbo.GamingGroupInvitation", "PlayerId");
+            //AddForeignKey("dbo.GamingGroupInvitation", "PlayerId", "dbo.Player", "Id");
         }
     }
 }
