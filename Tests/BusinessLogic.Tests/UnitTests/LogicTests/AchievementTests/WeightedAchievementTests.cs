70defba9949887a252551d6be2d1be45eaa38bf2
Brian Olmstead <monkeytorium@gmail.com>
2016-08-02 20:39:25 -0600

    Added Logic for Weighted Achievement Added unit tests for Weighted Achievement

---
+++ Tests/BusinessLogic.Tests/UnitTests/LogicTests/AchievementTests/WeightedAchievementTests.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BusinessLogic.DataAccess;
+using BusinessLogic.Logic.Achievements;
+using BusinessLogic.Logic.Points;
+using BusinessLogic.Models;
+using BusinessLogic.Models.Achievements;
+using NUnit.Framework;
+using Rhino.Mocks;
+using StructureMap.AutoMocking;
+
+namespace BusinessLogic.Tests.UnitTests.LogicTests.AchievementTests
+{
+    [TestFixture]
+    public class WeightedAchievementTests
+    {
+        private RhinoAutoMocker<WeightedAchievement> _autoMocker;
+        private readonly int _playerId = 1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _autoMocker = new RhinoAutoMocker<WeightedAchievement>();
+        }
+
+        [Test]
+        public void ItDoesNotAwardTheAchievementWhenThePlayerDoesNotReachTheBronzeThreshold()
+        {
+            //--arrange
+            SetupGamesForPlayer(_playerId, _autoMocker.ClassUnderTest.LevelThresholds[AchievementLevel.Bronze] - 1);
+
+            //--act
+            var results = _autoMocker.ClassUnderTest.IsAwardedForThisPlayer(_playerId);
+
+            //--assert
+            Assert.That(results.LevelAwarded, Is.Null);
+        }
+
+        [Test]
+        public void ItAwardsBronzeWhenPlayerHasExactlyBronzeNumberOfPlayedGames()
+        {
+            //--arrange
+            SetupGamesForPlayer(_playerId, _autoMocker.ClassUnderTest.LevelThresholds[AchievementLevel.Bronze]);
+
+            //--act
+            var results = _autoMocker.ClassUnderTest.IsAwardedForThisPlayer(_playerId);
+
+            //--assert
+            Assert.That(results.LevelAwarded, Is.EqualTo(AchievementLevel.Bronze));
+        }
+
+        [Test]
+        public void ItAwardsSilverWhenPlayerHasExactlySilverNumberOfPlayedGames()
+        {
+            //--arrange
+            SetupGamesForPlayer(_playerId, _autoMocker.ClassUnderTest.LevelThresholds[AchievementLevel.Silver]);
+
+            //--act
+            var results = _autoMocker.ClassUnderTest.IsAwardedForThisPlayer(_playerId);
+
+            //--assert
+            Assert.That(results.LevelAwarded, Is.EqualTo(AchievementLevel.Silver));
+        }
+
+        [Test]
+        public void ItAwardsGoldWhenPlayerHasExactlyGoldNumberOfPlayedGames()
+        {
+            //--arrange
+            SetupGamesForPlayer(_playerId, _autoMocker.ClassUnderTest.LevelThresholds[AchievementLevel.Gold]);
+
+            //--act
+            var results = _autoMocker.ClassUnderTest.IsAwardedForThisPlayer(_playerId);
+
+            //--assert
+            Assert.That(results.LevelAwarded, Is.EqualTo(AchievementLevel.Gold));
+        }
+
+        private void SetupGamesForPlayer(int playerId, int numberofUniqueGamesToSetupPerWeight)
+        {
+            var playerGameResults = new List<PlayerGameResult>();
+
+            for (int i = 0; i < numberofUniqueGamesToSetupPerWeight; i++)
+            {
+                AddPlayerGameResult(
+                    playerId,
+                    playerGameResults,
+                    i,
+                    decimal.Subtract(WeightTierCalculator.BOARD_GAME_GEEK_WEIGHT_INCLUSIVE_LOWER_BOUND_FOR_EASY, (decimal).1));
+
+                AddPlayerGameResult(
+                    playerId,
+                    playerGameResults,
+                    i,
+                    WeightTierCalculator.BOARD_GAME_GEEK_WEIGHT_INCLUSIVE_LOWER_BOUND_FOR_EASY);
+
+                AddPlayerGameResult(
+                    playerId,
+                    playerGameResults,
+                    i,
+                    WeightTierCalculator.BOARD_GAME_GEEK_WEIGHT_INCLUSIVE_LOWER_BOUND_FOR_ADVANCED);
+
+                AddPlayerGameResult(
+                    playerId,
+                    playerGameResults,
+                    i,
+                    WeightTierCalculator.BOARD_GAME_GEEK_WEIGHT_INCLUSIVE_LOWER_BOUND_FOR_CHALLENGING);
+
+                AddPlayerGameResult(
+                    playerId,
+                    playerGameResults,
+                    i,
+                    WeightTierCalculator.BOARD_GAME_GEEK_WEIGHT_INCLUSIVE_LOWER_BOUND_FOR_HARDCORE);
+            }
+
+            _autoMocker.Get<IDataContext>().Expect(mock => mock.GetQueryable<PlayerGameResult>()).Return(playerGameResults.AsQueryable());
+        }
+
+        private static void AddPlayerGameResult(
+            int playerId,
+            List<PlayerGameResult> playerGameResults,
+            int gameDefinitionId,
+            decimal averageWeight)
+        {
+            playerGameResults.Add(
+                new PlayerGameResult
+                {
+                    PlayerId = playerId,
+                    PlayedGame = new PlayedGame
+                    {
+                        GameDefinitionId = gameDefinitionId,
+                        GameDefinition = new GameDefinition
+                        {
+                            BoardGameGeekGameDefinition = new BoardGameGeekGameDefinition
+                            {
+                                AverageWeight = averageWeight
+                            }
+                        }
+                    }
+                });
+        }
+    }
+}
