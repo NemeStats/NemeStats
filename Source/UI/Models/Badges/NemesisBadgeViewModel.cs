a56ee3653a340ecab4b51ee08af636c58867239b
jakejgordon <jakejgordon@gmail.com>
2016-08-06 23:35:07 -0400

    Miscallenous change to update Nemesis description, clarying that you only need to rank higher than a given player rather than actually win the game to become somoene's Nemesis.

--- Source/UI/Models/Badges/NemesisBadgeViewModel.cs
+++ Source/UI/Models/Badges/NemesisBadgeViewModel.cs
@@ -5,7 +5,7 @@ namespace UI.Models.Badges
     public class NemesisBadgeViewModel : IBadgeBaseViewModel
     {
         public const string ICON_CSS_CLASS = "ns-icon-nemesis";
-        public const string POPOVER_TEXT = "This is the current Player's Nemesis. The Nemesis is the Player with the highest win % vs. this Player. Must have won at least 3 games to be designated as the Nemesis.";
+        public const string POPOVER_TEXT = "This is the current Player's Nemesis. The Nemesis is the Player with the highest win % vs. this Player. Must have ranked higher in at least 3 games to be designated as the Nemesis.";
 
         public string GetIconCssClass()
         {
