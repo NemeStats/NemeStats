6a80603b9746a7d2108de70598153b90d2d4288c
jakejgordon <jakejgordon@gmail.com>
2016-08-31 12:25:13 -0400

    now generates PlayedGameApplicationLinkage records for all existing PlayedGames

--- Source/BusinessLogic/Migrations/201608280145267_AddPlayedGameApplicationLinkage.cs
+++ Source/BusinessLogic/Migrations/201608280145267_AddPlayedGameApplicationLinkage.cs
@@ -1,6 +1,5 @@
 namespace BusinessLogic.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
     
     public partial class AddPlayedGameApplicationLinkage : DbMigration
@@ -19,8 +18,9 @@ namespace BusinessLogic.Migrations
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.PlayedGame", t => t.PlayedGameId, cascadeDelete: true)
                 .Index(t => new { t.PlayedGameId, t.ApplicationName, t.EntityId }, unique: true, name: "IX_PLAYEDGAMEID_APPLICATIONID_ENTITYID");
-            
-        }
+
+            //--create application linkages for all existing NemeStats games
+            Sql("INSERT INTO PlayedGameApplicationLinkage (PlayedGameId, ApplicationName, EntityId) SELECT PlayedGame.Id AS PlayedGameId, 'NemeStats' AS ApplicationName, PlayedGame.Id AS EntityId FROM PlayedGame");        }
         
         public override void Down()
         {
