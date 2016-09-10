6459e5d7d787d3cbed1cf811c1dc080911dd932a
Víctor Fernández Portero <vfportero@gmail.com>
2016-09-10 12:49:02 +0200

    Fixed Migration conflict

---
+++ Source/BusinessLogic/Migrations/201609101043083_2.50.cs
@@ -0,0 +1,20 @@
+namespace BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _250 : DbMigration
+    {
+        //Void migration to fix code first migration conflict between master & dice chucker branches
+        public override void Up()
+        {
+           
+            
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
