9c6c0a8af59b1e81c12a1b18e76cc7a97078f856
jakejgordon <jakejgordon@gmail.com>
2016-09-28 22:06:29 -0400

    Finished implementing most of the PlayerDeleter and left a big fat TODO for Brian to fill in the missing tests

--- Source/BusinessLogic/Logic/Players/PlayerDeleter.cs
+++ Source/BusinessLogic/Logic/Players/PlayerDeleter.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using AutoMapper.Internal;
 using BusinessLogic.DataAccess;
+using BusinessLogic.Logic.Champions;
+using BusinessLogic.Logic.Nemeses;
 using BusinessLogic.Models;
 using BusinessLogic.Models.User;
 
@@ -10,17 +14,36 @@ namespace BusinessLogic.Logic.Players
     public class PlayerDeleter : IPlayerDeleter
     {
         private readonly IDataContext _dataContext;
+        private readonly IChampionRecalculator _championRecalculator;
+        private readonly INemesisRecalculator _nemesisRecalculator;
 
-        public PlayerDeleter(IDataContext dataContext)
+        public PlayerDeleter(IDataContext dataContext, IChampionRecalculator championRecalculator, INemesisRecalculator nemesisRecalculator)
         {
             _dataContext = dataContext;
+            _championRecalculator = championRecalculator;
+            _nemesisRecalculator = nemesisRecalculator;
         }
 
         public void DeletePlayer(int playerId, ApplicationUser currentUser)
         {
+            ValidatePlayer(playerId);
+
+            DeletePlayerAchievements(playerId, currentUser);
+
+            DeleteChampionRecords(playerId, currentUser);
+
+            DeletePlayerNemesesRecords(playerId, currentUser);
+
+            _dataContext.DeleteById<Player>(playerId, currentUser);
+
+            _dataContext.CommitAllChanges();
+        }
+
+        private void ValidatePlayer(int playerId)
+        {
             var playerToDelete = _dataContext.GetQueryable<Player>()
-                .Include(p=>p.PlayerGameResults)
-                .FirstOrDefault(p=>p.Id == playerId);
+                .Include(p => p.PlayerGameResults)
+                .FirstOrDefault(p => p.Id == playerId);
 
             if (playerToDelete == null)
             {
@@ -30,27 +53,28 @@ namespace BusinessLogic.Logic.Players
             {
                 throw new Exception("You can not delete players with any played game");
             }
+        }
 
-            // Delete Player Achievements
-            var playerAchievementsToDelete = _dataContext.GetQueryable<PlayerAchievement>()
-                .Where(p => p.PlayerId == playerId)
+        private void DeletePlayerAchievements(int playerId, ApplicationUser currentUser)
+        {
+            var playerAchievementIds = _dataContext.GetQueryable<PlayerAchievement>()
+                 .Where(p => p.PlayerId == playerId)
+                .Select(p => p.Id)
                 .ToList();
 
-            foreach (var achieve in playerAchievementsToDelete)
-            {
-                var achievementId = achieve.Id;
-                _dataContext.DeleteById<PlayerAchievement>(achievementId, currentUser);
-            }
+            playerAchievementIds.ForEach(x => _dataContext.DeleteById<PlayerAchievement>(x, currentUser));
+        }
 
-            // Delete Player Champions
-            // TODO: Recalculate Champions for affected games?
+        private void DeleteChampionRecords(int playerId, ApplicationUser currentUser)
+        {
+            var gameDefinitionIdsThatNeedChampionRecalculated = new HashSet<int>();
             var playerChampionsToDelete = _dataContext.GetQueryable<Champion>()
                 .Where(p => p.PlayerId == playerId)
                 .ToList();
 
             foreach (var champion in playerChampionsToDelete)
             {
-                var championId = champion.Id; 
+                var championId = champion.Id;
 
                 var gameDefinitionsWithChampionToDelete = _dataContext.GetQueryable<GameDefinition>()
                     .Where(p => p.ChampionId == championId)
@@ -59,13 +83,23 @@ namespace BusinessLogic.Logic.Players
                 foreach (var gameDef in gameDefinitionsWithChampionToDelete)
                 {
                     gameDef.ChampionId = null;
+                    gameDefinitionIdsThatNeedChampionRecalculated.Add(gameDef.Id);
                 }
-               
+
                 _dataContext.DeleteById<Champion>(championId, currentUser);
             }
 
-            // Delete Player Nemeses
-            // TODO: Recalculate Nemeses for affected players?
+            RecalculateChampions(gameDefinitionIdsThatNeedChampionRecalculated, currentUser);
+        }
+
+        private void RecalculateChampions(IEnumerable<int> gameDefinitionIds, ApplicationUser currentUser)
+        {
+            gameDefinitionIds.Each(x => _championRecalculator.RecalculateChampion(x, currentUser));
+        }
+
+        private void DeletePlayerNemesesRecords(int playerId, ApplicationUser currentUser)
+        {
+            var playerIdsThatNeedNewNemesis = new HashSet<int>();
             var playerNemesesToDelete = _dataContext.GetQueryable<Nemesis>()
                 .Where(p => p.MinionPlayerId == playerId || p.NemesisPlayerId == playerId)
                 .ToList();
@@ -74,33 +108,46 @@ namespace BusinessLogic.Logic.Players
             {
                 var nemesisId = nemesisRecord.Id;
 
-                var playersToUpdatePreviousNemesis = _dataContext.GetQueryable<Player>()
-                    .Where(p => p.PreviousNemesisId == nemesisId)
-                    .ToList();
+                ClearOutPreviousNemesis(nemesisId, currentUser);
 
-                foreach (var player in playersToUpdatePreviousNemesis)
-                {
-                    player.PreviousNemesisId = null;
-                }
-
-                var playersToUpdateCurrentNemesis = _dataContext.GetQueryable<Player>()
-                    .Where(p => p.NemesisId == nemesisId)
-                    .ToList();
-
-                foreach (var player in playersToUpdateCurrentNemesis)
-                {
-                    player.NemesisId = null;
-                }
+                ClearOutCurrentNemesis(nemesisId, currentUser, playerIdsThatNeedNewNemesis);
 
                 _dataContext.DeleteById<Nemesis>(nemesisId, currentUser);
             }
 
-
-
-            // Delete Player
-            _dataContext.DeleteById<Player>(playerId, currentUser);
-            _dataContext.CommitAllChanges();
+            RecalculateNemesis(playerIdsThatNeedNewNemesis, currentUser);
         }
-        
+
+        private void ClearOutPreviousNemesis(int nemesisId, ApplicationUser currentUser)
+        {
+            var playersToUpdatePreviousNemesis = _dataContext.GetQueryable<Player>()
+                .Where(p => p.PreviousNemesisId == nemesisId)
+                .ToList();
+
+            foreach (var player in playersToUpdatePreviousNemesis)
+            {
+                player.PreviousNemesisId = null;
+                _dataContext.Save(player, currentUser);
+            }
+        }
+
+        private void ClearOutCurrentNemesis(int nemesisId, ApplicationUser currentUser, HashSet<int> playerIdsToClear)
+        {
+            var playersToUpdateCurrentNemesis = _dataContext.GetQueryable<Player>()
+                .Where(p => p.NemesisId == nemesisId)
+                .ToList();
+
+            foreach (var player in playersToUpdateCurrentNemesis)
+            {
+                player.NemesisId = null;
+                _dataContext.Save(player, currentUser);
+                playerIdsToClear.Add(player.Id);
+            }
+        }
+
+        private void RecalculateNemesis(IEnumerable<int> playerIdsThatNeedNewNemesis, ApplicationUser currentUser)
+        {
+            playerIdsThatNeedNewNemesis.Each((x => _nemesisRecalculator.RecalculateNemesis(x, currentUser)));
+        }
     }
 }
--- Tests/BusinessLogic.Tests/UnitTests/LogicTests/PlayersTests/PlayerDeleterTests/DeletePlayerTests.cs
+++ Tests/BusinessLogic.Tests/UnitTests/LogicTests/PlayersTests/PlayerDeleterTests/DeletePlayerTests.cs
@@ -7,28 +7,24 @@ using BusinessLogic.Models;
 using BusinessLogic.Models.User;
 using NUnit.Framework;
 using Rhino.Mocks;
+using StructureMap.AutoMocking;
 
 namespace BusinessLogic.Tests.UnitTests.LogicTests.PlayersTests.PlayerDeleterTests
 {
     [TestFixture]
     public class DeletePlayerTests
     {
-        protected IDataContext DataContextMock;
-        protected PlayerDeleter PlayerDeleter;
+        protected RhinoAutoMocker<PlayerDeleter> AutoMocker;
         protected ApplicationUser CurrentUser;
         protected int PlayerId = 1;
 
         [SetUp]
         public virtual void SetUp()
         {
-            DataContextMock = MockRepository.GenerateMock<IDataContext>();
-
-            PlayerDeleter = new PlayerDeleter(DataContextMock);
+            AutoMocker = new RhinoAutoMocker<PlayerDeleter>();
 
             CurrentUser = new ApplicationUser();
-
         }
-
     }
 
     public class When_Player_Not_Exists : DeletePlayerTests
@@ -37,7 +33,7 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PlayersTests.PlayerDeleterTes
         {
             base.SetUp();
 
-            DataContextMock.Expect(m => m.GetQueryable<Player>()).Return(new List<Player>().AsQueryable());
+            AutoMocker.Get<IDataContext>().Expect(m => m.GetQueryable<Player>()).Return(new List<Player>().AsQueryable());
         }
 
         [Test]
@@ -45,7 +41,7 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PlayersTests.PlayerDeleterTes
         {
             var expectedException = new ArgumentException("Player not exists","playerId");
 
-            var exception = Assert.Throws<ArgumentException>(() => PlayerDeleter.DeletePlayer(PlayerId, CurrentUser));
+            var exception = Assert.Throws<ArgumentException>(() => AutoMocker.ClassUnderTest.DeletePlayer(PlayerId, CurrentUser));
 
             Assert.AreEqual(expectedException.Message, exception.Message);
         }
@@ -62,7 +58,7 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PlayersTests.PlayerDeleterTes
                 new Player {Id = PlayerId, PlayerGameResults = new List<PlayerGameResult> {new PlayerGameResult()}}
             };
 
-            DataContextMock.Expect(m => m.GetQueryable<Player>()).Return(players.AsQueryable());
+            AutoMocker.Get<IDataContext>().Expect(m => m.GetQueryable<Player>()).Return(players.AsQueryable());
         }
 
         [Test]
@@ -70,7 +66,7 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PlayersTests.PlayerDeleterTes
         {
             var expectedException = new Exception("You can not delete players with any played game");
 
-            var exception = Assert.Throws<Exception>(() => PlayerDeleter.DeletePlayer(PlayerId, CurrentUser));
+            var exception = Assert.Throws<Exception>(() => AutoMocker.ClassUnderTest.DeletePlayer(PlayerId, CurrentUser));
 
             Assert.AreEqual(expectedException.Message, exception.Message);
         }
@@ -87,15 +83,24 @@ namespace BusinessLogic.Tests.UnitTests.LogicTests.PlayersTests.PlayerDeleterTes
                 new Player {Id = PlayerId, PlayerGameResults = new List<PlayerGameResult> ()}
             };
 
-            DataContextMock.Expect(m => m.GetQueryable<Player>()).Return(players.AsQueryable());
+            AutoMocker.Get<IDataContext>().Expect(m => m.GetQueryable<Player>()).Return(players.AsQueryable());
         }
 
         [Test]
         public void Then_Deletes_The_Player()
         {
-            PlayerDeleter.DeletePlayer(1, CurrentUser);
+            AutoMocker.ClassUnderTest.DeletePlayer(1, CurrentUser);
 
-            DataContextMock.AssertWasCalled(mock => mock.DeleteById<Player>(PlayerId, CurrentUser));
+            AutoMocker.Get<IDataContext>().AssertWasCalled(mock => mock.DeleteById<Player>(PlayerId, CurrentUser));
         }
     }
+
+    //TODO BRIAN need tests for at least the following
+    //* it deletes achievements
+    //* it deletes champions
+    //* it recalculates champions once per game definition
+    //* it only recalculates champions once (not more!) for a given game definition
+    //* it clears out the previous nemesis of any player who has this player as a previous nemesis
+    //* it clears out the current nemesis of any player who currently has this player as a nemesis
+    //* it recalculates the nemesis once (not more!) each player that had their current nemesis cleared
 }
