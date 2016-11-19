8d4d24a9d4bb5b46f631592e64bb8577d5b4964f
Víctor Fernández Portero <vfportero@gmail.com>
2016-11-19 13:22:27 +0100

    #450 - Edit played game from the new record played game view

---
+++ Source/UI/Mappers/CustomMappers/SavePlayedGameRequestToUpdatedGameGameMapper.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using AutoMapper;
+using BusinessLogic.Models.Games;
+using UI.Mappers.Interfaces;
+
+namespace UI.Mappers.CustomMappers
+{
+    public class SavePlayedGameRequestToUpdatedGameGameMapper : BaseCustomMapper<SavePlayedGameRequest, UpdatedGame>
+    {
+        private readonly CreatePlayerRankRequestToPlayerRankMapper _createPlayerRankRequestToPlayerRankMapper;
+
+        static SavePlayedGameRequestToUpdatedGameGameMapper()
+        {
+            Mapper.CreateMap<SavePlayedGameRequest, UpdatedGame>()
+                .ForMember(m => m.PlayerRanks, o => o.Ignore())
+                .ForMember(m => m.ApplicationLinkages, opt => opt.Ignore());
+        }
+
+        public SavePlayedGameRequestToUpdatedGameGameMapper(CreatePlayerRankRequestToPlayerRankMapper createPlayerRankRequestToPlayerRankMapper)
+        {
+            _createPlayerRankRequestToPlayerRankMapper = createPlayerRankRequestToPlayerRankMapper;
+        }
+
+        public override UpdatedGame Map(SavePlayedGameRequest source)
+        {
+            var result = base.Map(source);
+
+            result.PlayerRanks = _createPlayerRankRequestToPlayerRankMapper.Map(source.PlayerRanks).ToList();
+
+            return result;
+        }
+    }
+}
