d0df3a27723e37bfadc56d027cf465ee2a540865
jakejgordon <jakejgordon@gmail.com>
2017-10-10 07:39:45 -0400

    Created mechanism for action filters to use StructureMap dependency injection

---
+++ Source/UI/Attributes/GlobalFilterProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using StructureMap;
+using UI.Attributes.Filters;
+
+namespace UI.Attributes
+{
+    public class GlobalFilterProvider : IFilterProvider
+    {
+        private readonly IContainer _container;
+
+        public GlobalFilterProvider(IContainer container)
+        {
+            _container = container;
+        }
+
+        public IEnumerable<Filter> GetFilters(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
+        {
+            return new List<Filter>
+            {
+                //--at the moment, this is the only global filter that needs dependency injection
+                new Filter(_container.GetInstance<EnsureValidGamingGroupContextAttribute>(), FilterScope.Global, order: null)
+            };
+        }
+    }
+}
