78e43d82caccd04adb11fe14c8ca00d87aa95002
jakejgordon <jakejgordon@gmail.com>
2017-11-20 22:30:40 -0500

    Removed dependency on playedGameRetriever from within PlayerRetriever

---
+++ Source/BusinessLogic/Models/Players/HomePagePlayerSummary.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogic.Models.PlayedGames;
+using BusinessLogic.Models.Points;
+
+namespace BusinessLogic.Models.Players
+{
+    public class HomePagePlayerSummary
+    {
+        public int TotalGamesWon { get; set; }
+        public NemePointsSummary NemePointsSummary { get; set; }
+        public int TotalGamesPlayed { get; set; }
+        public int? PlayerId { get; set; }
+        public PlayedGameQuickStats LastGamingGroupPlayedGame { get; set; }
+    }
+}
