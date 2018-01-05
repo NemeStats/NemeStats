466a800277906a0f6a1369d19390357f0dad3223
jakejgordon <jakejgordon@gmail.com>
2018-01-05 12:11:50 -0500

    Added interface for the SecuredEntityValidatorFactory

---
+++ Source/BusinessLogic/DataAccess/Security/ISecuredEntityValidatorFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessLogic.DataAccess.Security
+{
+    public interface ISecuredEntityValidatorFactory
+    {
+        ISecuredEntityValidator MakeSecuredEntityValidator<TEntity>(IDataContext dataContext) where TEntity : class;
+    }
+}
