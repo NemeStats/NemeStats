d9188c777e4ba616605bdf197ff553865467c9fa
jakejgordon <jakejgordon@gmail.com>
2018-07-24 21:26:13 -0400

    added hover and clickable rows to played games tables

--- Source/UI/Scripts/GamingGroup/gamingGroup.js
+++ Source/UI/Scripts/GamingGroup/gamingGroup.js
@@ -293,7 +293,7 @@ Views.GamingGroup.GamingGroupView.prototype = {
             success: function (html) {
                 $("#" + divIdForRenderingResults).html(html);
 
-                var layout = new Views.Shared.Layout();
+                var layout = new window.Views.Shared.Layout();
                 layout.initializePopoversAndTooltips(parent);
 
                 var playedGamesView = new window.Views.PlayedGame.PlayedGames();
